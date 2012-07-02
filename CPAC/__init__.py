aae7932a9ba623e8cb184efb4d59ea8ba67461a0
Brian Cheung <bcheung@rocky.rfmh.org>
2012-07-02 14:14:15 -0400

    Fixed spelling

--- CPAC/__init__.py
+++ CPAC/__init__.py
@@ -2,7 +2,7 @@
 Configurable Pipeline for the Analysis of Connectomes
 =====================================================
 
-CPAC is a a configurable, open-source, Nipype-based, automated processing pipeline for resting state functional MRI (R-fMRI) data, for use by both novice and expert users.
+CPAC is a configurable, open-source, Nipype-based, automated processing pipeline for resting state functional MRI (R-fMRI) data, for use by both novice and expert users.
 """
 
 
