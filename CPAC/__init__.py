212498fb11b6821e210027a327a123dffa1e13e8
Brian Cheung <bcheung@rocky.rfmh.org>
2012-07-02 12:18:36 -0400

    Benchmarks folder added

--- CPAC/__init__.py
+++ CPAC/__init__.py
@@ -5,5 +5,15 @@ Configurable Pipeline for the Analysis of Connectomes
 CPAC is a a configurable, open-source, Nipype-based, automated processing pipeline for resting state functional MRI (R-fMRI) data, for use by both novice and expert users.
 """
 
+
+from numpy.testing import nosetester
+test = nosetester.NoseTester.test
+
+class _NoseTester(nosetester.NoseTester):
+    def test(self, label='fast', verbose=1, extra_argv=['--exe'], doctests = False, coverage=False):
+        return super(_NoseTester, self).test(label=label, verbose=verbose, extra_argv=extra_argv, doctests=doctests, coverage=coverage)
+    
+test = _NoseTester().test
+
 __all__ = ['anatpreproc', 'sca']
 __version__ = ['0.1-git']
