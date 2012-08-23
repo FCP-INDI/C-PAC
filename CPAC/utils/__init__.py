277b5a5ff9d334211251728fec7d525a5b80a7c5
ssikka <ssikka25@gmail.com>
2012-08-23 15:24:03 -0400

    added datasource files

--- CPAC/utils/__init__.py
+++ CPAC/utils/__init__.py
@@ -1,1 +1,4 @@
 from .utils import *
+
+from .anat_datasource import create_anat_datasource
+from .func_datasource import create_func_datasource
---
+++ CPAC/utils/anat_datasource.py
@@ -0,0 +1,21 @@
+
+def create_anat_datasource(wf_name='anat_datasource'):
+
+    import nipype.pipeline.engine as pe
+    import nipype.interfaces.utility as util
+
+    wf = pe.Workflow(name=wf_name)
+
+    inputnode = pe.Node(util.IdentityInterface(
+                                fields=['subject', 'anat'],
+                                mandatory_inputs=True),
+                        name='inputnode')
+
+    outputnode = pe.Node(util.IdentityInterface(fields=['subject',
+                                                     'anat' ]),
+                         name='outputspec')
+
+    wf.connect(inputnode, 'subject', outputnode, 'subject')
+    wf.connect(inputnode, 'anat', outputnode, 'anat')
+
+    return wf
