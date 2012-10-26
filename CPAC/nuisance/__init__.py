3c2102776a7efbcc72145d2bef3e0fea3cbf91dc
cow <briancheung>
2012-10-26 15:04:27 -0400

    Documentation and Git versioning functionality

--- CPAC/nuisance/__init__.py
+++ CPAC/nuisance/__init__.py
@@ -3,10 +3,12 @@ from utils import calc_compcor_components, \
 
 from nuisance import create_nuisance, \
                      calc_residuals, \
-                     bandpass_voxels
+                     bandpass_voxels, \
+                     extract_tissue_data
 
 __all__ = ['create_nuisance', \
            'calc_residuals', \
            'bandpass_voxels', \
            'calc_compcor_components', \
-           'erode_mask']
+           'erode_mask', \
+           'extract_tissue_data']
