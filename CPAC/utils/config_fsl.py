dff6b035f7b1b73b4ff48f869df732443a53e23f
Dan Lurie <cogito.ergo.dan@gmail.com>
2012-11-16 13:26:06 -0500

    Added comments to config_fsl.

--- CPAC/utils/config_fsl.py
+++ CPAC/utils/config_fsl.py
@@ -5,23 +5,20 @@ First Run
 # These names should match column names in template_phenotypic.csv
 columnsInModel = ['AgeAtScan', 'MeanFD', 'site', 'DxGroup']
 
+# Set a type for each of the variables/regressors entered above.
+# 1 = categorical, 0 = directional
+categoricalVsDirectional = [0, 0, 1, 1]
+
+# Specify whether to de-mean each column.
+deMean = [1, 1, 0, 0]
+
 # Specify the full path to template_phenotypic.csv
 phenotypicFile = '/path/to/template_phenotypic.csv'
 
 # Specify the full path to subject_list_group_analysis.txt
 subjectListFile = '/path/to/subject_list_group_analysis.txt'
 
-"""
-	Categorical :  1
-	Directional :  0
-"""
-
-# Set a type for each of the variables/regressors entered above.
-categoricalVsDirectional = [0, 0, 1, 1]
-
-deMean = [1, 1, 0, 0]
-
-
+# IGNORE - not yet implemented
 groupingVariable = ['DxGroup']
 
 # IGNORE - not yet implemented
@@ -29,6 +26,8 @@ modelGroupVariancesSeparately = 0
 
 outputModelFile = '/home/ssikka/nki_nyu_pipeline/model_fsl.csv'
 
+# Specify a name for the model.
 modelName = 'my_model'
 
+# Full path to a contrast file based on the columns listed above.
 contrastFile = '/home/ssikka/nki_nyu_pipeline/contrasts.csv'
