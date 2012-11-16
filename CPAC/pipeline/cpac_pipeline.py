ededa776b7c9d73f8a3fbff249cbc6d6f3369f5e
ssikka <ssikka25@gmail.com>
2012-11-16 18:21:48 -0500

    added fristons 24 parameter model

--- CPAC/pipeline/cpac_pipeline.py
+++ CPAC/pipeline/cpac_pipeline.py
@@ -421,6 +421,52 @@ def prep_workflow(sub_dict, c, strategies):
 
     strat_list += new_strat_list
 
+
+
+    """
+    Inserting Friston's 24 parameter  Workflow
+    Incase this workflow runs , it overwrites the movement_parameters file
+    So the file contains 24 parameters for motion and that gets wired to all the workflows
+    that depend on. The effect should be seen when regressing out nuisance signals and motion
+    is used as one of the regressors
+    """
+    new_strat_list = []
+    num_strat = 0
+
+    workflow_counter += 1
+    if 1 in c.usefristonModel:
+        workflow_bit_id['fristons_parameter_model'] = workflow_counter
+        for strat in strat_list:
+
+            fristons_model = fristons_twenty_four(wf_name='fristons_parameter_model_%d' % num_strat)
+
+            try:
+
+                node, out_file = strat.get_node_from_resource_pool('movement_parameters')
+                workflow.connect(node, out_file,
+                                 fristons_model, 'inputspec.movement_file')
+
+            except:
+                print 'Invalid Connection: fristons_parameter_model ', num_strat, ' resource_pool: ', strat.get_resource_pool()
+                raise
+
+            if 0 in c.usefristonModel:
+                tmp = strategy()
+                tmp.resource_pool = dict(strat.resource_pool)
+                tmp.leaf_node = (strat.leaf_node)
+                tmp.leaf_out_file = str(strat.leaf_out_file)
+                tmp.name = list(strat.name)
+                strat = tmp
+                new_strat_list.append(strat)
+            strat.append_name('fristons_parameter_model')
+
+            strat.update_resource_pool({'movement_parameters':(fristons_model, 'outputspec.movement_file')})
+
+            num_strat += 1
+    strat_list += new_strat_list
+
+
+
     """
     Inserting Anatomical to Functional Registration
     """
