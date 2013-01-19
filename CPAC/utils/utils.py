dbef44d91cf1cd1e196a2b229b6e008b4042a682
ssikka <ssikka25@gmail.com>
2013-01-18 20:05:52 -0500

    generate rois

--- CPAC/utils/utils.py
+++ CPAC/utils/utils.py
@@ -245,9 +245,7 @@ def create_seeds_(seed_specification_file, mask_directory_path, FSLDIR):
 
     seed_specifications = [line.rstrip('\r\n') for line in open(seed_specification_file, 'r').readlines() if not line.startswith('#')]
 
-    seed_files = []
 
-    index = 0
 
     seed_resolutions = {}
 
@@ -258,60 +256,72 @@ def create_seeds_(seed_specification_file, mask_directory_path, FSLDIR):
 
         if resolution not in seed_resolutions.keys():
             seed_resolutions[resolution] = []
-            seed_resolutions[resolution].append((seed_label, x, y, z, radius, resolution))
+        seed_resolutions[resolution].append((seed_label, x, y, z, radius, resolution))
+
 
 
     return_roi_files = []
     for resolution_key in seed_resolutions:
 
 
+        index = 0
+        seed_files = []
         for roi_set in seed_resolutions[resolution_key]:
+
 
             seed_label, x, y, z, radius, resolution = roi_set
             if not os.path.exists(mask_directory_path):
                 os.makedirs(mask_directory_path)
 
+            print 'checking if file exists ', '%s/data/standard/MNI152_T1_%s_brain.nii.gz' % (FSLDIR, resolution)
+            assert(os.path.exists('%s/data/standard/MNI152_T1_%s_brain.nii.gz' % (FSLDIR, resolution)) )
             cmd = "echo %s %s %s | 3dUndump -prefix %s.nii.gz -master %s/data/standard/MNI152_T1_%s_brain.nii.gz \
-            -srad %s -orient LPI -xyz -" % (x, y, z, os.path.join(mask_directory_path, str(index) +'_'+ seed_label +'_'+resolution), FSLDIR, resolution, radius)
+-srad %s -orient LPI -xyz -" % (x, y, z, os.path.join(mask_directory_path, str(index) +'_'+ seed_label +'_'+resolution), FSLDIR, resolution, radius)
 
+            print cmd
             try:
                 commands.getoutput(cmd)
-                seed_files.append(os.path.join(mask_directory_path, '%s.nii.gz' % (str(index) +'_'+ seed_label +'_'+resolution)), seed_label)
+                seed_files.append((os.path.join(mask_directory_path, '%s.nii.gz' % (str(index) +'_'+ seed_label +'_'+resolution)), seed_label))
+                print seed_files
             except:
                 raise
 
             index += 1
 
+        print index, ' ', seed_files
         seed_str = ' '
         intensities = ''
-        for idx in index:
+        for idx in range(0, index):
 
             seed, intensity = seed_files[idx]
 
             intensities += intensity+'_'
 
-            cmd = "3dcalc -a %s -expr 'a*%s' -prefix %s" % (seed, intensity, os.path.join(mask_directory_path, 'ic'+seed))
-
+            cmd = "3dcalc -a %s -expr 'a*%s' -prefix %s" % (seed, intensity, os.path.join(mask_directory_path, 'ic_'+ os.path.basename(seed)))
+            print cmd
             try:
                 commands.getoutput(cmd)
 
-                seed_str += "%s " % os.path.join(mask_directory_path, 'ic'+seed)
+                seed_str += "%s " % os.path.join(mask_directory_path, 'ic_'+ os.path.basename(seed))
             except:
                 raise
 
 
         cmd = '3dMean  -prefix %s.nii.gz -sum %s' % (os.path.join(mask_directory_path, 'rois_'+resolution), seed_str)
+        print cmd
         try:
             commands.getoutput(cmd)
         except:
             raise
 
         try:
-            cmd = 'rm -f %s' seed_str
+            cmd = 'rm -f %s' % seed_str
+            print cmd
             commands.getoutput(cmd)
             for seed, intensity in seed_files:
                 try:
                     cmd  = 'rm -f  '+ seed
+                    print cmd
                     commands.getoutput(cmd)
                 except:
                     raise
