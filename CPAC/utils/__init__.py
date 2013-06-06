8ee67b02fdea009bdbf5a332f45bf61a06896f2c
ranjitk <ranjit.jhu@gmail.com>
2013-06-06 19:57:30 -0400

    added extract parameters to utils

--- CPAC/utils/__init__.py
+++ CPAC/utils/__init__.py
@@ -1,8 +1,8 @@
 from utils import *
-from create_all_qc import run
+# from .create_all_qc import run
 from .extract_data import run
-from .extract_data_multiscan import run
-from .create_fsl_model import run
+# from .extract_data_multiscan import run
+# from .create_fsl_model import run
 from .datasource import create_anat_datasource
 from .datasource import create_func_datasource
 from .datasource import create_mask_dataflow
