f6fe9718c9720261b591ac2a8df44b6542a025e6
sgiavasis <sgiavasis@ieee.org>
2013-07-19 17:50:59 -0400

    Introduced create_ants_nonlinear_xfm declaration

--- CPAC/registration/__init__.py
+++ CPAC/registration/__init__.py
@@ -1,7 +1,9 @@
 from registration import create_nonlinear_register, \
                          create_register_func_to_mni, \
-                         create_bbregister_func_to_anat
+                         create_bbregister_func_to_anat, \
+                         create_ants_nonlinear_xfm
 
 __all__ = ['create_nonlinear_register', \
            'create_register_func_to_mni', \
-           'create_bbregister_func_to_anat']
+           'create_bbregister_func_to_anat', \
+           'create_ants_nonlinear_xfm']
