91ddee62f47eee6dfae5c70dc96f70af7beebfbe
sgiavasis <sgiavasis@ieee.org>
2013-07-31 10:20:11 -0400

    Included temporary fix which removes pre-existing CPAC installs before installing new version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import sys
-
-
+ 
+ 
 DISTNAME = 'CPAC'
 DESCRIPTION = 'Configurable Pipeline for the Analysis of Connectomes'
 LONG_DESCRIPTION = ''
@@ -13,31 +13,65 @@ DOWNLOAD_URL = ''
 
 import CPAC
 VERSION = CPAC.__version__
-
+ 
+print "Version: ", VERSION
+ 
 from numpy.distutils.core import setup
 
 def configuration(parent_package='', top_path=None):
     from numpy.distutils.misc_util import Configuration
+ 
     config = Configuration(None, parent_package, top_path)
-    
+     
     config.set_options(ignore_setup_xxx_py=True,
                        assume_default_configuration=True,
                        delegate_options_to_subpackages=True,
                        quiet=True)
-    
+     
     config.add_subpackage('CPAC')
-
-
+ 
+ 
     return config
+ 
+if __name__ == "__main__": 
+    
+    import site
+    import shutil
+    from shutil import rmtree
+    from shutil import copytree
 
-if __name__ == "__main__":
+    # Temporary code to remove pre-existing CPAC directories
+    # before re-installing - needs more elegant solution
+
+    for sitePath in site.getsitepackages():
+
+        for root,dirs,files in os.walk(sitePath):
+            
+            if 'CPAC-backup' in root:
+                shutil.rmtree(root)
+
+
+    for sitePath in site.getsitepackages():
+
+        for root,dirs,files in os.walk(sitePath):
+            
+            if 'CPAC' in root:
+                backupPath = sitePath + '/CPAC-backup'
+
+                shutil.copytree(root,backupPath)
+                shutil.rmtree(root)
+                print "Backing up pre-existing CPAC directory into ", backupPath
+                print "Removing directory ", root
+
+ 
     old_path = os.getcwd()
     local_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+ 
     
     os.chdir(local_path)
     sys.path.insert(0, local_path)
-    
-    setup(configuration=configuration, 
+     
+    setup(configuration=configuration,
           scripts=['scripts/log_py2js.py', 'scripts/cpac_gui'],
           name=DISTNAME,
           maintainer=MAINTAINER,
@@ -51,4 +85,4 @@ if __name__ == "__main__":
           classifiers=['Intended Audience :: Science/Research',
                        'Programming Language :: Python'
                       ])
-    
+
