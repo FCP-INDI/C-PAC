9abc608eda90668e7ca3fb2c2f631a4d68aa87e7
sgiavasis <sgiavasis@ieee.org>
2013-08-09 16:35:05 -0400

    Updated version number.

--- CPAC/__init__.py
+++ CPAC/__init__.py
@@ -47,7 +47,7 @@ from subprocess import Popen, PIPE
 import re
 
 try:
-    version = '0.3.2'
+    version = '0.3.1'
 #    gitproc = Popen(['git', 'log', '--oneline'], stdout = PIPE)
 #    (stdout, stderr) = gitproc.communicate()
 #    rows = stdout.split("\n")
