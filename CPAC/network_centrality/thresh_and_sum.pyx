8d93f5caa744c8d037d53c01088a12d74dc1ce42
Zarrar Shehzad <czarrar@gmail.com>
2014-03-06 17:41:54 -0500

    fixing/optimizing degree calculation

--- CPAC/network_centrality/thresh_and_sum.pyx
+++ CPAC/network_centrality/thresh_and_sum.pyx
@@ -66,14 +66,14 @@ def transform_weighted_double(np.ndarray[double, ndim=2] cmat):
 def centrality_binarize_float(np.ndarray[float, ndim=2] cmat, np.ndarray[float, ndim=1] cent, float thresh):
     cdef unsigned int i,j
     for i in xrange(cmat.shape[0]):
-        for j in xrange(i,cmat.shape[1]):
-            cent[i] = cent[i] + 1*(cmat[i,j] > thresh)
+        for j in xrange(cmat.shape[1]):
+            cent[i] += 1.0*(cmat[i,j] > thresh)
 
 def centrality_binarize_double(np.ndarray[double, ndim=2] cmat, np.ndarray[double, ndim=1] cent, double thresh):
     cdef unsigned int i,j
     for i in xrange(cmat.shape[0]):
         for j in xrange(cmat.shape[1]):
-            cent[i] = cent[i] + 1*(cmat[i,j] > thresh)
+            cent[i] += 1.0*(cmat[i,j] > thresh)
 
 # Weighted
 def centrality_weighted_float(np.ndarray[float, ndim=2] cmat, np.ndarray[float, ndim=1] cent, float thresh):
@@ -94,11 +94,11 @@ def centrality_both_float(np.ndarray[float, ndim=2] cmat, np.ndarray[float, ndim
     for i in xrange(cmat.shape[0]):
         for j in xrange(cmat.shape[1]):
             cent_uwt[i] = cent_uwt[i] + cmat[i,j]*(cmat[i,j] > thresh)
-            cent_wt[i] = cent_wt[i] + 1*(cmat[i,j] > thresh)
+            cent_wt[i] = cent_wt[i] + 1.0*(cmat[i,j] > thresh)
 
 def centrality_both_double(np.ndarray[double, ndim=2] cmat, np.ndarray[double, ndim=1] cent_bin, np.ndarray[double, ndim=1] cent_wt, double thresh):
     cdef unsigned int i,j
     for i in xrange(cmat.shape[0]):
         for j in xrange(cmat.shape[1]):
             cent_bin[i] = cent_bin[i] + cmat[i,j]*(cmat[i,j] > thresh)
-            cent_wt[i] = cent_wt[i] + 1*(cmat[i,j] > thresh)
+            cent_wt[i] = cent_wt[i] + 1.0*(cmat[i,j] > thresh)
