bbdb4c29332bdee860eda6a0e4855b08113ce676
sgiavasis <sgiava77@gmail.com>
2014-12-11 13:01:50 -0500

    Removed some fTest code (for now) that may cause problems.

--- CPAC/pipeline/cpac_group_analysis_pipeline.py
+++ CPAC/pipeline/cpac_group_analysis_pipeline.py
@@ -32,6 +32,8 @@ def prep_group_analysis_workflow(c, resource, subject_infos):
 
     p_id, s_ids, scan_ids, s_paths = (list(tup) for tup in zip(*subject_infos))
 
+    # set this to False for now
+    fTest = False
 
     def get_phenotypic_file(phenotypic_file, m_dict, m_list, mod_path, sub_id):
         
@@ -370,7 +372,7 @@ def prep_group_analysis_workflow(c, resource, subject_infos):
         try:
 
             from CPAC.utils import create_fsl_model
-            create_fsl_model.run(conf, False, parameter_file, pipeline_path, current_output, True)
+            create_fsl_model.run(conf, fTest, parameter_file, pipeline_path, current_output, True)
 
             #print >>diag, "> Runs create_fsl_model."
             #print >>diag, ""
@@ -568,14 +570,14 @@ def prep_group_analysis_workflow(c, resource, subject_infos):
 
         gp_flow = create_grp_analysis_dataflow("gp_dataflow_%s" % currentDerivative)
         gp_flow.inputs.inputspec.grp_model = model
-        gp_flow.inputs.inputspec.ftest = c.fTest
+        gp_flow.inputs.inputspefTest = fTest
   
 
 
         # gpa_wf
         # Creates the actual group analysis workflow
 
-        gpa_wf = create_group_analysis(c.fTest, "gp_analysis_%s" % currentDerivative)
+        gpa_wf = create_group_analysis(fTest, "gp_analysis_%s" % currentDerivative)
 
         gpa_wf.inputs.inputspec.zmap_files = ordered_paths
         gpa_wf.inputs.inputspec.z_threshold = c.zThreshold
@@ -583,7 +585,7 @@ def prep_group_analysis_workflow(c, resource, subject_infos):
         gpa_wf.inputs.inputspec.parameters = (c.FSLDIR, 'MNI152')
     
         print "group model: ", model
-        print "f test: ", c.fTest
+        print "f test: ", fTest
         print "z threshold: ", c.zThreshold
         print "p threshold: ", c.pThreshold
         print "parameters: ", (c.FSLDIR, 'MNI152')
@@ -597,7 +599,7 @@ def prep_group_analysis_workflow(c, resource, subject_infos):
                     gpa_wf, 'inputspec.grp_file')
 
             
-        if c.fTest:
+        if fTest:
             wf.connect(gp_flow, 'outputspec.fts',
                        gpa_wf, 'inputspec.fts_file')
         
@@ -658,7 +660,7 @@ def prep_group_analysis_workflow(c, resource, subject_infos):
 
 
         ########datasink connections#########
-        if c.fTest:
+        if fTest:
             wf.connect(gp_flow, 'outputspec.fts',
                        ds, 'model_files.@0') 
         
