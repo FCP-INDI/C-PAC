a649127ed6d8c80b16c80d8e7b4e6fe7552c2b8b
John Pellman <pellman.john@gmail.com>
2015-04-03 18:15:08 -0400

    Updated in-app documentation

--- CPAC/GUI/resources/html/_sources/conf.py
+++ CPAC/GUI/resources/html/_sources/conf.py
@@ -131,7 +131,7 @@ html_theme_options = {"nosidebar":"true"}
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {'**': ['globaltoc.html', 'searchbox.html'] }
+html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
--- doc/workflows/anat_preproc.rst
+++ doc/workflows/anat_preproc.rst
@@ -2,4 +2,4 @@ Anatomical Preprocessing
 ========================
 
 .. currentmodule:: anat_preproc
-.. autofunction:: create_anat_preproc
+.. autofunction:: CPAC.anat_preproc.create_anat_preproc
