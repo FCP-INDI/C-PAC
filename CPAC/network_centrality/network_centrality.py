df80e77e7ae65e10cdfaece2a7820778b7efbbaa
dclark87 <danieljclark87@gmail.com>
2015-10-15 19:55:39 -0400

    Added in multiplying the eigenvalue to the eigenvector output

--- CPAC/network_centrality/network_centrality.py
+++ CPAC/network_centrality/network_centrality.py
@@ -122,11 +122,11 @@ def calc_eigen_from_1d(one_d_file, num_threads, mask_file):
                                                which=which_eigs, maxiter=max_iter)
 
     # Create eigenvector tuple
-    centrality_tuple = ('eigenvector_centrality_binarized', np.abs(bin_eig_vect))
+    centrality_tuple = ('eigenvector_centrality_binarized', np.abs(bin_eig_val*bin_eig_vect))
     bin_outfile = utils.map_centrality_matrix(centrality_tuple, mask_affine,
                                                 mask_arr, template_type)
 
-    centrality_tuple = ('eigenvector_centrality_weighted', np.abs(wght_eig_vect))
+    centrality_tuple = ('eigenvector_centrality_weighted', np.abs(wght_eig_val*wght_eig_vect))
     wght_outfile = utils.map_centrality_matrix(centrality_tuple, mask_affine,
                                                 mask_arr, template_type)
 
