e62776ce073943310b56fbcec49130b585b7c769
nrajamani3 <nrajamani3@gatech.edu>
2018-06-18 13:57:00 -0400

    typo. small

--- CPAC/anat_preproc/anat_preproc.py
+++ CPAC/anat_preproc/anat_preproc.py
@@ -6,10 +6,7 @@ import nipype.interfaces.utility as util
 
 
 def create_3dskullstrip_arg_string(shrink_fac, var_shrink_fac,
-                                   shrink_fac_bot_lim, avoid_vent, niter,
-                                   pushout, touchup, fill_hole, avoid_eyes,
-                                   use_edge,exp_frac, smooth_final, push_to_edge,
-                                   use_skull, perc_int, max_inter_iter, blur_fwhm, fac):
+                                   shrink_fac_bot_lim, avoid_vent,niter,pushout,touchup,fill_hole,avoid_eyes,use_edge,exp_frac,smooth_final,push_to_edge,use_skull,perc_int,max_inter_iter,blur_fwhm,fac):
 
     if var_shrink_fac:
         var_shrink_str = '-var_shrink_fac'
@@ -17,9 +14,14 @@ def create_3dskullstrip_arg_string(shrink_fac, var_shrink_fac,
         var_shrink_str = '-no_var_shrink_fac'
 
     if avoid_vent:
-        avoid_vent_str = '-avoid_vent'
+        avoid_vent_str = '-avoid_vent'   
+    
+    #elif avoid_vent == "Twice":
+     #   avoid_vent_str = '-avoid_vent -avoid_vent'
+    
     else:
         avoid_vent_str = '-no_avoid_vent'
+
     if pushout:
         pushout_str = '-pushout'
     else:
@@ -50,27 +52,26 @@ def create_3dskullstrip_arg_string(shrink_fac, var_shrink_fac,
     else:
         push_to_edge_str = '-no_push_to_edge'
 
-    expr = '-shrink_fac {0} ' \
-           '{1} ' \
-           '-shrink_fac_bot_lim {2} ' \
-           '{3} ' \
-           '-niter {4} ' \
-           '{5} ' \
-           '{6} ' \
-           '-fill_hole {7} ' \
-           '{8} ' \
-           '{9} ' \
-           '-exp_frac {10} ' \
-           '-smooth_final {11} ' \
-           '{12} ' \
-           '{13} ' \
-           '-perc_int {14} ' \
-           '-max_inter_iter {15} ' \
-           '-blur_fwhm {16} ' \
-           '-fac {17} '.format(shrink_fac, var_shrink_str, shrink_fac_bot_lim, avoid_vent_str,
-                               niter, pushout_str, touchup_str, fill_hole, avoid_eyes_str, use_edge_str,
-                               exp_frac, smooth_final, push_to_edge_str, use_skull_str, perc_int,
-                               max_inter_iter, blur_fwhm, fac)
+
+
+    expr = '-shrink_fac {0}'\
+           '{1}' \
+           '-shrink_fac_bot_lim {2}'\
+           '-avoid_vent{3}'\
+           '-niter {4}'\
+           '{5}'\
+           '{6}' \
+           '-fill_hole {7}'\
+           '{8}'\
+           '{9}'\
+           '-exp_frac {10}'\
+           '-smooth_final {11}'\
+           '{12}' \
+           '{13}' \
+           '-perc_int{14}' \
+           '-max_inter_iter{15}' \
+           '-blur_fwhm{16}' \
+           '-fac{17}'.format(shrink_fac,var_shrink_str,shrink_fac_bot_lim,avoid_vent_str, niter,pushout_str,touchup_str,fill_hole,avoid_eyes_str,use_edge_str,exp_frac,smooth_final,push_to_edge_str,use_skull_str,perc_int,max_inter_iter,blur_fwhm,fac)
 
     return expr
 
