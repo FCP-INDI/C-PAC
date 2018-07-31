a65598f2882a6b1193439ba238b8e8357458c0d2
anibalsolon <anibalsolon@gmail.com>
2018-07-31 16:21:14 -0400

    update import

--- CPAC/connectome/__init__.py
+++ CPAC/connectome/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from .pipeline import (FunctionalConnectivity, create_connectivity)
+from .pipeline import (FunctionalConnectivity, create_connectome_study)
