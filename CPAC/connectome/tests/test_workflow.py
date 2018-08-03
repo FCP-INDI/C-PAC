240be14ea189a476023e7ea1c062853cdfe6655b
anibalsolon <anibalsolon@gmail.com>
2018-08-03 17:15:23 -0400

    adjust workflow

--- CPAC/connectome/tests/test_workflow.py
+++ CPAC/connectome/tests/test_workflow.py
@@ -22,9 +22,11 @@ connectome:
 
   folds: 2
 
+  report:
+
   roi:
-    - type: aal
-      parameters:
+    # - type: aal
+    #   parameters:
 
     # - type: harvard_oxford
     #   parameters:
@@ -35,135 +37,135 @@ connectome:
     # - type: basc
     #   parameters:
 
-    # - type: ward
-    #   parameters: 40
+    - type: ward
+      parameters:
+        components: 40
+    - type: ward
+      parameters:
+        components: 60
+    - type: ward
+      parameters:
+        components: 80
+    - type: ward
+      parameters:
+        components: 100
+    - type: ward
+      parameters:
+        components: 120
+    - type: ward
+      parameters:
+        components: 150
+    - type: ward
+      parameters:
+        components: 200
+    - type: ward
+      parameters:
+        components: 300
 
-    # - type: ward
-    #   parameters: 60
+    - type: k_means
+      parameters:
+        components: 40
+    - type: k_means
+      parameters:
+        components: 60
+    - type: k_means
+      parameters:
+        components: 80
+    - type: k_means
+      parameters:
+        components: 100
+    - type: k_means
+      parameters:
+        components: 120
+    - type: k_means
+      parameters:
+        components: 150
+    - type: k_means
+      parameters:
+        components: 200
+    - type: k_means
+      parameters:
+        components: 300
 
-    # - type: ward
-    #   parameters: 80
+    - type: group_ica
+      parameters:
+        components: 40
+    - type: group_ica
+      parameters:
+        components: 60
+    - type: group_ica
+      parameters:
+        components: 80
+    - type: group_ica
+      parameters:
+        components: 100
+    - type: group_ica
+      parameters:
+        components: 120
 
-    # - type: ward
-    #   parameters: 100
-
-    # - type: ward
-    #   parameters: 120
-
-    # - type: ward
-    #   parameters: 150
-
-    # - type: ward
-    #   parameters: 200
-
-    # - type: ward
-    #   parameters: 300
-
-    # - type: k_means
-    #   parameters: 40
-
-    # - type: k_means
-    #   parameters: 60
-
-    # - type: k_means
-    #   parameters: 80
-
-    # - type: k_means
-    #   parameters: 100
-
-    # - type: k_means
-    #   parameters: 120
-
-    # - type: k_means
-    #   parameters: 150
-
-    # - type: k_means
-    #   parameters: 200
-
-    # - type: k_means
-    #   parameters: 300
-
-    # - type: group_ica
-    #   parameters:
-    #     components: 40
-    # - type: group_ica
-    #   parameters:
-    #     components: 60
-    # - type: group_ica
-    #   parameters:
-    #     components: 80
-    # - type: group_ica
-    #   parameters:
-    #     components: 100
-    # - type: group_ica
-    #   parameters:
-    #     components: 120
-
-    # - type: dict_learning
-    #   parameters:
-    #     components: 40
-    # - type: dict_learning
-    #   parameters:
-    #     components: 60
-    # - type: dict_learning
-    #   parameters:
-    #     components: 80
-    # - type: dict_learning
-    #   parameters:
-    #     components: 100
-    # - type: dict_learning
-    #   parameters:
-    #     components: 120
+    - type: dict_learning
+      parameters:
+        components: 40
+    - type: dict_learning
+      parameters:
+        components: 60
+    - type: dict_learning
+      parameters:
+        components: 80
+    - type: dict_learning
+      parameters:
+        components: 100
+    - type: dict_learning
+      parameters:
+        components: 120
 
   connectivity: 
     - type: partial
       parameters:
-
-    # - type: tangent
-    #   parameters:
-
-    # - type: correlation
-    #   parameters:
+    - type: tangent
+      parameters:
+    - type: correlation
+      parameters:
 
   classifier:
 
     - type: knn
       parameters:
 
-    # - type: random_forest
-    #   parameters:
+    - type: random_forest
+      parameters:
 
-    # - type: naive_bayes
-    #   parameters:
+    - type: naive_bayes
+      parameters:
 
-    # - type: ridge
-    #   parameters:
+    - type: ridge
+      parameters:
 
-    # - type: svc
-    #   parameters:
-    #     penalty: l1
-    #     anova_percentile: 10
+    - type: svc
+      parameters:
+        penalty: l1
+        anova_percentile: 10
           
-    # - type: svc
-    #   parameters:
-    #     penalty: l2
-    #     anova_percentile: 10
+    - type: svc
+      parameters:
+        penalty: l2
+        anova_percentile: 10
 
-    # - type: svc
-    #   parameters:
-    #     penalty: l1
+    - type: svc
+      parameters:
+        penalty: l1
 
-    # - type: svc
-    #   parameters:
-    #     penalty: l2
+    - type: svc
+      parameters:
+        penalty: l2
 
-    # - type: logistic
-    #   parameters:
-    #     penalty: l1
+    - type: logistic
+      parameters:
+        penalty: l1
 
-    # - type: logistic
-    #   parameters:
-    #     penalty: l2
+    - type: logistic
+      parameters:
+        penalty: l2
 
     """)
 
