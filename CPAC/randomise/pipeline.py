5ecf2c4125466b599209b4174ed9740fd536957e
nrajamani3 <nrajamani3@gatech.edu>
2018-11-14 14:11:43 -0500

    updated

--- CPAC/randomise/pipeline.py
+++ CPAC/randomise/pipeline.py
@@ -1,7 +1,6 @@
 import nipype.pipeline.engine as pe
 import nipype.interfaces.utility as util
-from nipype.interfaces.base import BaseInterface, \
-    BaseInterfaceInputSpec, traits, File, TraitedSpec
+from nipype.interfaces.base import BaseInterface, BaseInterfaceInputSpec, traits, File, TraitedSpec
 from nipype.interfaces import fsl
 
 from nilearn import input_data, masking, image, datasets
@@ -16,14 +15,22 @@ import numpy as np
 import nibabel as nb
 
 
+
 def select(input_list):
-    out_file = input_list[0]
-    return out_file
+    import nibabel as nb
+    for i in input_list:
+        img = nb.load(i)
+        hdr = img.header
+        if hdr['cal_min'] == 0 and hdr['cal_max'] == 0:
+            print ("Warning! {} is an empty image because of no positive values in the unpermuted statistic image, and it could not be processed with tfce.".format('i'))
+        if not hdr['cal_max'] == 0 and hdr['cal_min'] == 0:
+            selected_file = i
 
+    return i
 
-def create_randomise(name='randomise', working_dir=None, crash_dir=None):
+def create_randomise(name='randomise',working_dir=None,crash_dir=None):
+
     """
-    
     Parameters
     ----------
         
@@ -56,112 +63,84 @@ def create_randomise(name='randomise', working_dir=None, crash_dir=None):
     wf.config['execution'] = {'hash_method': 'timestamp',
                               'crashdump_dir': os.path.abspath(crash_dir)}
 
-    inputspec = pe.Node(
-        util.IdentityInterface(fields=[
-            'subjects',
-            'design_matrix_file',
-            'constrast_file',
-            'f_constrast_file',
-            'permutations',
-        ]),
-        name='inputspec'
-    )
+    inputspec = pe.Node(util.IdentityInterface(fields=['subjects','design_matrix_file','constrast_file','permutations']),name='inputspec')
 
-    outputspec = pe.Node(
-        util.IdentityInterface(fields=[
-            'index_file'
-            'threshold_file'
-            'localmax_txt_file'
-            'localmax_vol_file'
-            'max_file'
-            'mean_file'
-            'pval_file'
-            'size_file'
-        ]),
-        name='outputspec'
-    )
+    outputspec = pe.Node(util.IdentityInterface(fields=['tstat_files' ,'t_corrected_p_files','index_file','threshold_file','localmax_txt_file','localmax_vol_file','max_file','mean_file','pval_file','size_file']), name='outputspec')
 
-    merge = pe.Node(interface=fsl.Merge(), name='fsl_merge')
-    merge.inputs.dimension = 't'
-    merge.inputs.merged_file = "randomise_merged.nii.gz"
+    #merge = pe.Node(interface=fsl.Merge(), name='fsl_merge')
+    #merge.inputs.dimension = 't'
+    #merge.inputs.merged_file = "randomise_merged.nii.gz"
 
-    wf.connect(inputspec, 'subjects', merge, 'in_files')
+    #wf.connect(inputspec, 'subjects', merge, 'in_files')
 
-    mask = pe.Node(interface=fsl.maths.MathsCommand(), name='fsl_maths')
-    mask.inputs.args = '-abs -Tmin -bin'
-    mask.inputs.out_file = "randomise_mask.nii.gz"
-
-    wf.connect(merge, 'merged_file', mask, 'in_file')
+    #mask = pe.Node(interface=fsl.maths.MathsCommand(), name='fsl_maths')
+    #mask.inputs.args = '-abs -Tmin -bin'
+    #mask.inputs.out_file = "randomise_mask.nii.gz"
+    #wf.connect(inputspec, 'subjects', mask, 'in_file')
 
     randomise = pe.Node(interface=fsl.Randomise(), name='randomise')
-    wf.connect(mask, 'out_file', randomise, 'mask')
     randomise.inputs.base_name = "randomise"
     randomise.inputs.demean = True
     randomise.inputs.tfce = True
-    wf.connect([
-        (merge, randomise, [('merged_file', 'in_file')]),
-        (inputspec, randomise, [
+    wf.connect([(inputspec, randomise, [('subjects', 'in_file'),
             ('design_matrix_file', 'design_mat'),
             ('constrast_file', 'tcon'),
-            ('f_constrast_file', 'fcon'),
             ('permutations', 'num_perm'),
-        ]),
-    ])
+            ])])
+    wf.connect(randomise,'tstat_files',outputspec,'tstat_files')
+    wf.connect(randomise,'t_corrected_p_files',outputspec,'t_corrected_p_files')
+#------------- issue here arises while using tfce. By not using tfce, you don't get t_corrected_p files. R V in a conundrum? --------------------#
 
-    select_t_corrected = pe.Node(Function(input_names=["input_list"],
-                                          output_names=['out_file'],
-                                          function=select),
-                                 name='select_t_cor')
+    
+    select_tcorrp_files = pe.Node(Function(input_names=['input_list'],output_names=['out_file'],function=select),name='select_t_corrp')
 
-    wf.connect(randomise, "t_corrected_p_files",
-               select_t_corrected, "input_list")
+    wf.connect(randomise, 't_corrected_p_files',select_tcorrp_files, 'input_list')
+    wf.connect(select_tcorrp_files,'out_file',outputspec,'out_tcorr_corrected')
 
-    thresh = pe.Node(interface=fsl.Threshold(),
-                     name='fsl_threshold_contrast')
+    select_tstat_files = pe.Node(Function(input_names=['input_list'],output_names=['out_file'],function=select),name='select_t_stat')
+
+    wf.connect(randomise, 'tstat_files',select_tstat_files, 'input_list')
+    wf.connect(select_tstat_files,'out_file',outputspec,'out_tstat_corrected')
+
+    thresh = pe.Node(interface=fsl.Threshold(),name='fsl_threshold_contrast')
     thresh.inputs.thresh = 0.95
     thresh.inputs.out_file = 'rando_pipe_thresh_tstat.nii.gz'
+    wf.connect(select_tstat_files, 'out_file', thresh, 'in_file')
+    wf.connect(thresh,'out_file',outputspec,'rando_pipe_thresh_tstat.nii.gz')
 
-    wf.connect(select_t_corrected, "out_file", thresh, "in_file")
+    thresh_bin = pe.Node(interface=fsl.UnaryMaths(),name='fsl_threshold_bin_contrast')
+    thresh_bin.inputs.operation = 'bin'
+    wf.connect(thresh, 'out_file', thresh_bin, 'in_file')
+    wf.connect(thresh_bin,'out_file',outputspec,'thresh_bin_out')
 
-    thresh_bin = pe.Node(interface=fsl.maths.MathsCommand(),
-                         name='fsl_threshold_bin_contrast')
-    thresh_bin.inputs.args = '-bin'
-    wf.connect(thresh, "out_file", thresh_bin, "in_file")
-
-    select_t_stat = pe.Node(Function(input_names=["input_list"],
-                                     output_names=['out_file'],
-                                     function=select),
-                            name='select_item_t_stat')
-
-    wf.connect(randomise, "tstat_files", select_t_stat, "input_list")
-
-    apply_mask = pe.Node(interface=fsl.ApplyMask(),
-                         name='fsl_applymask_contrast')
-    wf.connect(select_t_stat, 'out_file', apply_mask, 'in_file')
+    apply_mask = pe.Node(interface=fsl.ApplyMask(),name='fsl_applymask_contrast')
+    wf.connect(select_tstat_files, 'out_file', apply_mask, 'in_file')
     wf.connect(thresh_bin, 'out_file', apply_mask, 'mask_file')
 
-    cluster = pe.Node(interface=fsl.Cluster(),
-                      name='cluster_contrast')
+
+    cluster = pe.Node(interface=fsl.Cluster(),name='cluster_contrast')
     cluster.inputs.threshold = 0.0001
-    cluster.inputs.out_index_file = "cluster_index_contrast"
+    cluster.inputs.out_index_file = "index_file"
     cluster.inputs.out_localmax_txt_file = "lmax_contrast.txt"
     cluster.inputs.out_size_file = "cluster_size_contrast"
-    cluster.inputs.out_threshold_file = "randomise_out_contrast"
-    cluster.inputs.terminal_output = 'file'
+    cluster.inputs.out_threshold_file = True
+    cluster.inputs.out_max_file = True
+    cluster.inputs.out_mean_file = True
+    cluster.inputs.out_pval_file = True
+    cluster.inputs.out_size_file = True
+    
 
     wf.connect(apply_mask, 'out_file', cluster, 'in_file')
-
-    wf.connect([
-        (cluster, outputspec, [
-            ('index_file', 'index_file')
-            ('threshold_file', 'threshold_file')
-            ('localmax_txt_file', 'localmax_txt_file')
-            ('localmax_vol_file', 'localmax_vol_file')
-            ('max_file', 'max_file')
-            ('mean_file', 'mean_file')
-            ('pval_file', 'pval_file')
-            ('size_file', 'size_file')
-        ])
-    ])
+    
+    wf.connect(cluster,'index_file',outputspec,'index_file')
+    wf.connect(cluster,'threshold_file',outputspec,'threshold_file')
+    wf.connect(cluster,'localmax_txt_file',outputspec,'localmax_txt_file')
+    wf.connect(cluster,'localmax_vol_file',outputspec,'localmax_vol_file')
+    wf.connect(cluster,'max_file',outputspec,'max_file')
+    wf.connect(cluster,'mean_file',outputspec,'meal_file')
+    wf.connect(cluster,'pval_file',outputspec,'pval_file')
+    wf.connect(cluster,'size_file',outputspec,'size_file')
+    
+  
 
     return wf
