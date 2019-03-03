019d6df89eb0187392633072dc974f9bf6651322
anibalsolon <anibalsolon@gmail.com>
2019-03-03 17:15:55 -0500

    fix motion stats init

--- CPAC/generate_motion_statistics/__init__.py
+++ CPAC/generate_motion_statistics/__init__.py
@@ -3,6 +3,13 @@ from generate_motion_statistics import (motion_power_statistics,
                                         calculate_FD_J,
                                         gen_motion_parameters,
                                         gen_power_parameters,
-                                        calculate_DVARS,
-                                        fristons_twenty_four,
-                                        calc_friston_twenty_four)
+                                        calculate_DVARS)
+
+__all__ = [
+    'motion_power_statistics',
+    'calculate_FD_P',
+    'calculate_FD_J',
+    'gen_motion_parameters',
+    'gen_power_parameters',
+    'calculate_DVARS'
+]
