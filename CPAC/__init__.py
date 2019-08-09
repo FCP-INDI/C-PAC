98daf60fbb929d0af6b2ac05669434526129bdbb
anibalsolon <anibalsolon@gmail.com>
2019-08-09 18:27:33 -0400

    Revert "not relative import"
    This reverts commit 983e46a9481a2e08e9f5190b6c70e2cea76d54d0.

--- CPAC/__init__.py
+++ CPAC/__init__.py
@@ -7,5 +7,39 @@ for resting state functional MRI (R-fMRI) data, for use by both novice and
 expert users.
 """
 
-from info import __version__
+import anat_preproc, \
+       aroma, \
+       EPI_DistCorr, \
+       connectome, \
+       func_preproc, \
+       reho, \
+       seg_preproc, \
+       registration, \
+       sca, \
+       nuisance, \
+       generate_motion_statistics, \
+       alff, \
+       qc, \
+       seg_preproc, \
+       vmhc, \
+       median_angle, \
+       timeseries, \
+       network_centrality, \
+       scrubbing, \
+       group_analysis, \
+       randomise, \
+       easy_thresh,\
+       utils, \
+       pipeline, \
+       cwas, \
+       qpp, \
+       GUI
+
+__all__ = ['GUI', 'pipeline', 'anat_preproc', 'func_preproc', 'epi_distcorr',
+           'registration', 'seg_preproc', 'reho', 'sca', 'nuisance',
+           'alff', 'vmhc', 'median_angle', 'generate_motion_statistics',
+           'timeseries', 'network_centrality', 'scrubbing', 'utils',
+           'group_analysis','randomise', 'easy_thresh', 'aroma', 'qc', 'qpp']
+
+from .info import __version__
 version = __version__
