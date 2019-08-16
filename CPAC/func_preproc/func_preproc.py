8ad191dc72c7b7932eed9793354cdbdcd03e294e
anibalsolon <anibalsolon@gmail.com>
2019-08-16 11:26:28 -0400

    use input transformer instead of node

--- CPAC/func_preproc/func_preproc.py
+++ CPAC/func_preproc/func_preproc.py
@@ -571,13 +571,18 @@ def slice_timing_wf(name='slice_timing'):
     # AFNI 3dTshift - needed this so the tpattern file
     # output from get_scan_params would be tied downstream
     # via a connection (to avoid poofing)
-    add_prefix = pe.Node(util.Function(input_names=['tpattern'],
-                                       output_names=['afni_prefix'],
-                                       function=add_afni_prefix),
-                         name='slice_timing_add_afni_prefix')
-    wf.connect(inputNode, 'tpattern', add_prefix, 'tpattern')
-    wf.connect(add_prefix, 'afni_prefix',
-               func_slice_timing_correction, 'tpattern')
+    wf.connect([
+        (
+            inputNode,
+            func_slice_timing_correction,
+            [
+                (
+                    ('tpattern', add_afni_prefix),
+                    'tpattern'
+                )
+            ]
+        ),
+    ])
 
     wf.connect(func_slice_timing_correction, 'out_file',
                outputNode, 'slice_time_corrected')
