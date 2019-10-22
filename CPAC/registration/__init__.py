a85377b8a113ee1947b547bef239b2e0003bafd3
Cameron Craddock <cameron.craddock@gmail.com>
2019-10-22 21:34:49 +0000

    remove deprecated functions, fsl_to_itk and collect transforms

--- CPAC/registration/__init__.py
+++ CPAC/registration/__init__.py
@@ -4,9 +4,6 @@ from registration import create_fsl_flirt_linear_reg, \
                          create_register_func_to_anat, \
                          create_bbregister_func_to_anat, \
                          create_wf_calculate_ants_warp
-                         #create_wf_apply_ants_warp, \
-                         #create_wf_c3d_fsl_to_itk, \
-                         #create_wf_collect_transform
 
 from output_func_to_standard import output_func_to_standard
 
@@ -16,7 +13,4 @@ __all__ = ['create_fsl_flirt_linear_reg', \
            'create_register_func_to_anat', \
            'create_bbregister_func_to_anat', \
            'create_wf_calculate_ants_warp', \
-           'create_wf_apply_ants_warp', \
-           'create_wf_c3d_fsl_to_itk', \
-           'create_wf_collect_transforms', \
            'output_func_to_standard']
