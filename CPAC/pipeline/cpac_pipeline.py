87941f0565ec171041df1e19a061d9815b6da3ac
Xinhui Li <sucyli1017@gmail.com>
2019-11-19 15:59:54 -0500

    delete comment

--- CPAC/pipeline/cpac_pipeline.py
+++ CPAC/pipeline/cpac_pipeline.py
@@ -439,7 +439,7 @@ def prep_workflow(sub_dict, c, run, pipeline_timing_info=None,
                                               function = resolve_resolution,
                                               as_module = True), 
                                         name = 'resampled_' + template_name) 
-                                        
+
         resampled_template.inputs.resolution = resolution
         resampled_template.inputs.template = template
         resampled_template.inputs.template_name = template_name
@@ -2970,25 +2970,7 @@ def prep_workflow(sub_dict, c, run, pipeline_timing_info=None,
                     })
 
                     # create the graphs
-                    from CPAC.utils.ndmg_utils import (
-                        # ndmg_roi_timeseries,
-                        ndmg_create_graphs
-                    )
-
-                    # ndmg_ts = pe.Node(Function(
-                    #     input_names=['func_file',
-                    #                  'label_file'],
-                    #     output_names=['roi_ts',
-                    #                   'rois',
-                    #                   'roits_file'],
-                    #     function=ndmg_roi_timeseries,
-                    #     as_module=True
-                    # ), name='ndmg_ts_%d' % num_strat)
-
-                    # workflow.connect(resample_functional_to_roi, 'out_file',
-                    #                  ndmg_ts, 'func_file')
-                    # workflow.connect(roi_dataflow, 'outputspec.out_file',
-                    #                  ndmg_ts, 'label_file')
+                    from CPAC.utils.ndmg_utils import ndmg_create_graphs
 
                     ndmg_graph = pe.MapNode(Function(
                         input_names=['ts', 'labels'],
