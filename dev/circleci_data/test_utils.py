87b9e9e7fd185e68819ee446d86c7f33313f28b7
Jon Clucas <jon@vasegurt.com>
2020-03-26 16:06:29 -0400

    :microscope: Test utils data_config_build

--- dev/circleci_data/test_utils.py
+++ dev/circleci_data/test_utils.py
@@ -6,17 +6,17 @@ import sys
 
 sys.path.append(CPAC_DIR)
 
-import click
 import os
-import pytest
+
+from CPAC.__main__ import utils as CPAC_main_utils
+
+DATA_DIR = os.path.join(CPAC_DIR, 'dev', 'circleci_data')
 
 def test_build_data_config(cli_runner):
-    from CPAC.__main__ import utils as CPAC_main_utils
-    data_dir = os.path.join(CPAC_DIR, 'dev', 'circleci_data')
-    
-    os.chdir(data_dir)
-    test_yaml = os.path.join(data_dir, "data_settings.yml")
+    os.chdir(DATA_DIR)
+    test_yaml = os.path.join(DATA_DIR, "data_settings.yml")
     _delete_test_yaml(test_yaml)
+    
     result = cli_runner.invoke(
         CPAC_main_utils.commands['data_config'].commands[
             'new_settings_template'
@@ -29,6 +29,28 @@ def test_build_data_config(cli_runner):
     )
     assert os.path.exists(test_yaml)
     _delete_test_yaml(test_yaml)
+    
+    
+def test_new_settings_template(cli_runner):
+    os.chdir(CPAC_DIR)
+    
+    result = cli_runner.invoke(
+        CPAC_main_utils.commands['data_config'].commands['build'],
+        [os.path.join(
+            DATA_DIR,
+            "data_settings_bids_examples_ds051_default_BIDS.yml"
+        )]
+    )
+    
+    participant_yaml = os.path.join(DATA_DIR, "data_config_ds051.yml")
+    group_yaml = os.path.join(DATA_DIR, "group_analysis_participants_ds051.txt")
+    
+    assert result.exit_code == 0
+    assert result.output.startswith("\nGenerating data configuration file..")
+    assert os.path.exists(participant_yaml)
+    assert os.path.exists(group_yaml)
+    _delete_test_yaml(participant_yaml)
+    _delete_test_yaml(group_yaml)
     
 def _delete_test_yaml(test_yaml):
     if os.path.exists(test_yaml):
