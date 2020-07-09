db71134f5d7a8eb8297a3418114a4ee633091a4a
root <root@ip-172-31-14-181.ec2.internal>
2020-07-09 23:25:54 +0000

    Functions for TR splitting for 3dvolreg parallelization.

--- CPAC/func_preproc/utils.py
+++ CPAC/func_preproc/utils.py
@@ -1,4 +1,7 @@
 
+import nibabel as nb
+import subprocess
+
 def add_afni_prefix(tpattern):
     if tpattern:
         if ".txt" in tpattern:
@@ -13,3 +16,37 @@ def nullify(value, function=None):
     if function:
         return function(value)
     return value
+
+
+def chunk_ts(func_file):
+    func_img = nb.load(func)
+    trs = func_img.shape[3]
+    chunk = trs/n_cpus
+    TR_ranges = []
+
+    for chunk_idx in range(0, n_cpus):
+        if chunk_idx == n_cpus - 1:
+            TR_ranges.append((chunk_idx*chunk, trs - 1))
+        else:
+            TR_ranges.append((chunk_idx*chunk, (chunk_idx+1)*chunk -1))
+    return TR_ranges
+
+
+def split_ts_chunks(func_file, tr_ranges):
+    if '.nii' in func_file:
+        ext = '.nii'
+    if '.nii.gz' in func_file:
+        ext = '.nii.gz'
+
+    split_funcs = []
+    for chunk_idx, tr_range in enumerate(tr_ranges):
+        out_file = os.path.join(os.getcwd(), os.path.basename(func_file).replace(ext, "_{0}{1}".format(chunk_idx, ext)))
+        in_file = "{0}[{1}..{2}]".format(func_file, tr_range[0], tr_range[1])
+
+        cmd = ["3dcalc", "-a", in_file, "-expr", "a", "-prefix", out_file]
+
+        retcode = subprocess.check_output(cmd)
+
+        split_funcs.append(out_file)
+
+    return split_funcs
