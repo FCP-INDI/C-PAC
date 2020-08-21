84a717a3520f35bb617cdf89c9c8a94e062b06f4
Hecheng Jin <hj2469@columbia.edu>
2020-08-21 00:24:18 -0400

    acpc uses its own template, instead of resampled template_skull_for_anat

--- CPAC/pipeline/cpac_pipeline.py
+++ CPAC/pipeline/cpac_pipeline.py
@@ -1199,12 +1199,10 @@ def build_workflow(subject_id, sub_dict, c, pipeline_name=None, num_ants_cores=1
                     node, out_file = new_strat['anatomical']
                     workflow.connect(node, out_file,
                                     anat_preproc, 'inputspec.anat')
-                    node, out_file = new_strat['template_brain_for_anat']
-                    workflow.connect(node, out_file,
+                    workflow.connect(c.acpc_template_skull, 'local_path',
+                                    anat_preproc, 'inputspec.template_skull_for_anat')                               
+                    workflow.connect(c.acpc_template_brain, 'local_path',
                                     anat_preproc, 'inputspec.template_brain_only_for_anat')
-                    node, out_file = new_strat['template_skull_for_anat']
-                    workflow.connect(node, out_file,
-                                    anat_preproc, 'inputspec.template_skull_for_anat')
                     new_strat.append_name(anat_preproc.name)
                     new_strat.set_leaf_properties(anat_preproc, 'outputspec.brain')
                     new_strat.update_resource_pool({
