3c8866bc72bcc7c6b6449cf73e0e84e6ebd9e689
Jon Clucas <jon@vasegurt.com>
2020-08-21 13:48:17 -0400

    :wrench: Run coverage as module

--- dev/circleci_data/test_in_image.sh
+++ dev/circleci_data/test_in_image.sh
@@ -4,5 +4,5 @@ pip install -r /code/dev/circleci_data/requirements.txt
 # create conftest.py in root
 printf "collect_ignore = ['run.py']\ncollect_ignore_glob = ['**/run.py', '**/test_mdmr_cython.py', '**/test_install.py']" > /code/conftest.py
 
-# run test with coverage
-coverage run -m pytest --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules dev/circleci_data/ /code
+# run test with coverage as module
+python -m coverage run -m pytest --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules dev/circleci_data/ /code
