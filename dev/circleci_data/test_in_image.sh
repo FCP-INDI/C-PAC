5b42f4b19a7cf5eddac64a144df72d562dde463c
Jon Clucas <jon@vasegurt.com>
2020-08-21 13:40:13 -0400

    :wrench: Configure pytest

--- dev/circleci_data/test_in_image.sh
+++ dev/circleci_data/test_in_image.sh
@@ -1,2 +1,8 @@
+# install testing requirements
 pip install -r /code/dev/circleci_data/requirements.txt
-coverage run -m pytest --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules --ignore=CPAC/cwas/tests/test_mdmr_cython.py --ignore=dev/circleci_data/test_install.py dev/circleci_data/ /code
+
+# create conftest.py in root
+printf "collect_ignore = ['run.py']\ncollect_ignore_glob = ['**/run.py', '**/test_mdmr_cython.py', '**/test_install.py']" > /code/conftest.py
+
+# run test with coverage
+coverage run -m pytest --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules dev/circleci_data/ /code
