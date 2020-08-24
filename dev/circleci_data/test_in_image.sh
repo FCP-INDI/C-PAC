b68734a63efdc926d9a1e0399eb4cb3e52001360
Jon Clucas <jon@vasegurt.com>
2020-08-24 16:05:16 -0400

    :construction_worker: Ignore test_install when testing in images

--- dev/circleci_data/test_in_image.sh
+++ dev/circleci_data/test_in_image.sh
@@ -2,4 +2,4 @@
 pip install -r /code/dev/circleci_data/requirements.txt
 
 # run test with coverage as module
-python -m coverage run -m pytest --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules dev/circleci_data/CPAC /code/CPAC
+python -m coverage run -m pytest --ignore-glob=*test_install.py --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules dev/circleci_data /code/CPAC
