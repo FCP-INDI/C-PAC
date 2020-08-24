cded6b5ae477f8f2bca8ca8ef998f0e427115355
Jon Clucas <jon@vasegurt.com>
2020-08-24 11:31:38 -0400

    :construction_worker: Set testing root to /code/CPAC

--- dev/circleci_data/test_in_image.sh
+++ dev/circleci_data/test_in_image.sh
@@ -1,8 +1,5 @@
 # install testing requirements
 pip install -r /code/dev/circleci_data/requirements.txt
 
-# create conftest.py in root
-printf "collect_ignore = ['run.py']\ncollect_ignore_glob = ['**/run.py', '**/test_mdmr_cython.py', '**/test_install.py']" > /code/conftest.py
-
 # run test with coverage as module
-python -m coverage run -m pytest --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules dev/circleci_data/ /code
+python -m coverage run -m pytest --junitxml=test-results/junit.xml --doctest-ignore-import-errors --continue-on-collection-errors --doctest-modules dev/circleci_data/CPAC /code/CPAC
