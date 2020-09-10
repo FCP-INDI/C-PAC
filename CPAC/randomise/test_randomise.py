4fcc98a2bcd13d9612124e1ac503ed8191629b21
Jon Clucas <jon@vasegurt.com>
2020-09-10 13:07:54 -0400

    :white_check_mark: Convert randomize from manual test to automatic
    And mark to skip, needing refactor

--- CPAC/randomise/test_randomise.py
+++ CPAC/randomise/test_randomise.py
@@ -2,10 +2,17 @@ import os
 import glob
 import nipype.interfaces.io as nio
 import nipype.pipeline.engine as pe
+import pytest
+
 from nipype.interfaces.fsl import ImageStats
 
 
-def run_randomize(inputs,output_dir=None,run=True):
+@pytest.mark.skip(reason="test needs refactoring")
+@pytest.mark.parametrize(
+     'inputs',
+     [['subjects', 'design_matrix_file', 'contrast_file', 'permutations']]
+)
+def test_run_randomize(inputs,output_dir=None,run=True):
    from . import pipeline
    randomise_workflow = pe.Workflow(name = 'preproc')
    if output_dir == None:
@@ -43,5 +50,3 @@ def run_randomize(inputs,output_dir=None,run=True):
        randomise_workflow.run(plugin='MultiProc', plugin_args ={'n_procs': num_of_cores})
    else:
        return randomise_workflow, randomise_workflow.base_dir
-
-run_randomize(['subjects','design_matrix_file','constrast_file','permutations'],output_dir=None,run=True)
