b072f9cb52c6a00c99197cf4c051390f63b79a58
Jon Clucas <jon@vasegurt.com>
2020-11-02 16:30:27 -0500

    :bookmark: Set semver to 1.7.3.dev

--- CPAC/info.py
+++ CPAC/info.py
@@ -10,8 +10,8 @@ This script was borrowed from and inspired by nipype's info.py file.
 # version
 _version_major = 1
 _version_minor = 7
-_version_micro = 1
-_version_extra = ''
+_version_micro = 3
+_version_extra = 'dev'
 
 
 def get_cpac_gitversion():
