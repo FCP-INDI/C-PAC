5a9db2b8bb5e60e17a28b3041392911e0908261b
Jon Clucas <jon@vasegurt.com>
2020-12-01 12:37:41 -0500

    :hammer: Simplify ANTs build script

--- dev/docker_data/install_ants.sh
+++ dev/docker_data/install_ants.sh
@@ -27,10 +27,9 @@ Build will proceed in 5 seconds
 sleep 5
 
 mkdir -p /antsinstall
-cd /antsinstall
 
-workingDir=${PWD}
-
+workingDir=/antsinstall
+cd ${workingDir}
 # Clone the repo
 git clone https://github.com/ANTsX/ANTs.git
 
@@ -50,86 +49,7 @@ mkdir -p $buildDir $installDir
 
 cd $buildDir
 
-# USE_VTK must be turned on to build antsSurf
-cmake \
-    -DCMAKE_INSTALL_PREFIX=$installDir \
-    -DBUILD_SHARED_LIBS=OFF \
-    -DUSE_VTK=OFF \
-    -DSuperBuild_ANTS_USE_GIT_PROTOCOL=OFF \
-    -DBUILD_TESTING=OFF \
-    -DRUN_LONG_TESTS=OFF \
-    -DRUN_SHORT_TESTS=OFF \
-    ${workingDir}/ANTs 2>&1 | tee cmake.log
-
-if [[ $? -ne 0 ]]; then
-  echo "ANTs SuperBuild configuration failed. Please review documentation at
-
-    $antsBuildInstructions
-
-  If opening an issue, please attach
-  
-  ${buildDir}/cmake.log
-  ${buildDir}/CMakeCache.txt
-  ${buildDir}/CMakeFiles/CMakeError.log
-  ${buildDir}/CMakeFiles/CMakeOutput.log
-  
-"
-  exit 1 
-fi
-
-make -j $buildThreads 2>&1 | tee build.log
-
-if [[ ! -f "CMakeFiles/ANTS-complete" ]]; then
-  echo "ANTs compilation failed. Please review documentation at
-
-    $antsBuildInstructions
-
-  If opening an issue, please attach
-
-  ${buildDir}/build.log
-  ${buildDir}/cmake.log
-  ${buildDir}/CMakeCache.txt
-  ${buildDir}/CMakeFiles/CMakeError.log
-  ${buildDir}/CMakeFiles/CMakeOutput.log
-  
-"
-  exit 1
-fi
-
+cmake ${workingDir}/ANTs
+make 2>&1 | tee build.log
 cd ANTS-build
-make install 2>&1 | tee install.log
-
-antsRegExe="${installDir}/bin/antsRegistration"
-
-if [[ ! -f ${antsRegExe} ]]; then
-  echo "Installation failed. Please review documentation at
-
-    $antsBuildInstructions
-
-  If opening an issue, please attach
-
-  ${buildDir}/build.log
-  ${buildDir}/cmake.log
-  ${buildDir}/CMakeCache.txt
-  ${buildDir}/ANTS-build/install.log
-  ${buildDir}/CMakeFiles/CMakeError.log
-  ${buildDir}/CMakeFiles/CMakeOutput.log
-
-"
-  exit 1
-fi
-
-echo "Installation complete, running ${antsRegExe}"
-
-${antsRegExe} --version
-
-echo "
-Binaries and scripts are located in 
-
-  $installDir
-
-Please see post installation instructions at 
-
-  $antsBuildInstructions
-
-"
+CMAKE_INSTALL_PREFIX=${installDir} make install 2>&1 | tee install.log
