9a9151f2acb575082bbee924a33953fa34d2f42b
Xinhui Li <sucyli1017@gmail.com>
2021-01-05 10:45:54 -0500

    fix "template_epi_mask" already in RP error

--- CPAC/pipeline/cpac_pipeline.py
+++ CPAC/pipeline/cpac_pipeline.py
@@ -821,7 +821,7 @@ def build_workflow(subject_id, sub_dict, c, pipeline_name=None, num_ants_cores=1
         (c.resolution_for_func_preproc, c.template_epi, 'template_epi', 'resolution_for_func_preproc'),  # no difference of skull and only brain
         (c.resolution_for_func_preproc, c.template_epi_mask, 'template_epi_mask', 'resolution_for_func_preproc'),  
         (c.resolution_for_func_derivative, c.template_epi, 'template_epi_derivative', 'resolution_for_func_derivative'),  # no difference of skull and only brain
-        (c.resolution_for_func_derivative, c.template_epi_mask, 'template_epi_mask', 'resolution_for_func_derivative'),  
+        (c.resolution_for_func_derivative, c.template_epi_mask, 'template_epi_mask_derivative', 'resolution_for_func_derivative'),  
         (c.resolution_for_func_derivative, c.template_brain_only_for_func, 'template_brain_for_func_derivative', 'resolution_for_func_preproc'),
         (c.resolution_for_func_derivative, c.template_skull_for_func, 'template_skull_for_func_derivative', 'resolution_for_func_preproc'),
     ]
