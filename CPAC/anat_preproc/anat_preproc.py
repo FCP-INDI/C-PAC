52e9a3e103a32010b94d83916b0a2a61e4848d23
Xinhui Li <sucyli1017@gmail.com>
2021-01-20 21:45:13 -0500

    change gca version

--- CPAC/anat_preproc/anat_preproc.py
+++ CPAC/anat_preproc/anat_preproc.py
@@ -868,12 +868,12 @@ def abcd_reconall(T1wImage, T1wImageFile, T1wImageBrainFile, SubjectID, SubjectD
     # mri_em_register -mask "$SubjectDIR"/"$SubjectID"/mri/brainmask.mgz "$SubjectDIR"/"$SubjectID"/mri/nu.mgz $FREESURFER_HOME/average/RB_all_2008-03-26.gca "$SubjectDIR"/"$SubjectID"/mri/transforms/talairach_with_skull.lta
     nu_path = os.path.join(SubjectDIR, SubjectID, 'mri/nu.mgz')
     talairach_with_skull_path = os.path.join(SubjectDIR, SubjectID, 'mri/transforms/talairach_with_skull.lta')
-    gca_path = '$FREESURFER_HOME/average/RB_all_2020-01-02.gca'
+    gca_path = '$FREESURFER_HOME/average/RB_all_2008-03-26.gca' # RB_all_2020-01-02.gca
     cmd = 'mri_em_register -mask %s %s %s %s' % (brainmask_path, nu_path, gca_path, talairach_with_skull_path)
     os.system(cmd)
 
     # mri_watershed -T1 -brain_atlas $FREESURFER_HOME/average/RB_all_withskull_2008-03-26.gca "$SubjectDIR"/"$SubjectID"/mri/transforms/talairach_with_skull.lta "$SubjectDIR"/"$SubjectID"/mri/T1.mgz "$SubjectDIR"/"$SubjectID"/mri/brainmask.auto.mgz 
-    gca_withskull_path = '$FREESURFER_HOME/average/RB_all_withskull_2020_01_02.gca'
+    gca_withskull_path = '$FREESURFER_HOME/average/RB_all_withskull_2008-03-26.gca' # RB_all_withskull_2020_01_02.gca
     T1_path = os.path.join(SubjectDIR, SubjectID, 'mri/T1.mgz')
     brainmask_auto_path = os.path.join(SubjectDIR, SubjectID, 'mri/brainmask.auto.mgz')
     cmd = 'mri_watershed -T1 -brain_atlas %s %s %s %s' % (gca_withskull_path, talairach_with_skull_path, T1_path, brainmask_auto_path)
