8b45e77101bd81773ca2c240de62817325c25c25
Jon Clucas <jon.clucas@childmind.org>
2021-01-27 16:31:40 -0500

    :white_check_mark: Add tests for overridden Node and MapNode
    Ref https://miykael.github.io/nipype_tutorial/notebooks/basic_mapnodes.html
    
    Co-authored-by: Michael Notter <michaelnotter@hotmail.com>

--- CPAC/pipeline/nipype_pipeline_engine/engine.py
+++ CPAC/pipeline/nipype_pipeline_engine/engine.py
@@ -10,13 +10,52 @@ from functools import partialmethod
 DEFAULT_MEM_GB = 2.0
 
 
+def _doctest_skiplines(docstring, lines_to_skip):
+    '''
+    Function to add '  # doctest: +SKIP' to the end of docstring lines
+    to skip in imported docstrings.
+
+    Parameters
+    ----------
+    docstring: str
+
+    lines_to_skip: set or list
+
+    Returns
+    -------
+    docstring: str
+
+    Examples
+    --------
+    >>> _doctest_skiplines('skip this line', {'skip this line'})
+    'skip this line  # doctest: +SKIP'
+    '''
+    if (
+        not isinstance(lines_to_skip, set) and
+        not isinstance(lines_to_skip, list)
+    ):
+        raise TypeError(
+            '_doctest_skiplines: `lines_to_skip` must be a set or list.')
+
+    return '\n'.join([
+        f'{line}  # doctest: +SKIP' if line in lines_to_skip else line
+        for line in docstring.split('\n')
+    ])
+
+
 class Node(pe.Node):
-    __doc__ = pe.Node.__doc__
+    __doc__ = _doctest_skiplines(
+        pe.Node.__doc__,
+        {"    >>> realign.inputs.in_files = 'functional.nii'"}
+    )
 
     __init__ = partialmethod(pe.Node.__init__, mem_gb=DEFAULT_MEM_GB)
 
 
 class MapNode(pe.MapNode):
-    __doc__ = f'kwarg default: mem_gb={DEFAULT_MEM_GB}\n\n{pe.MapNode.__doc__}'
+    __doc__ = _doctest_skiplines(
+        f'mem_gb={DEFAULT_MEM_GB}\n\n{pe.MapNode.__doc__}',
+        {"    ...                           'functional3.nii']"}
+    )
 
     __init__ = partialmethod(pe.MapNode.__init__, mem_gb=DEFAULT_MEM_GB)
