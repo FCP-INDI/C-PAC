62d72784e5bec65eba1c5ea453dc746fbf4c8dbd
Jon Clucas <jon.clucas@childmind.org>
2021-02-01 10:54:27 -0500

    fixup! fixup! :white_check_mark: Fix / skip cwas tests

--- CPAC/cwas/tests/test_mdmr_cython.py
+++ CPAC/cwas/tests/test_mdmr_cython.py
@@ -11,6 +11,7 @@ def test_mdmr():
 
     X = X.reshape((X.shape[0], X.shape[1], 1))
 
-    F_value, p_value = calc_cwas(X, Y, np.array([0, 1, 2], dtype=int), 500, [0])
+    F_value, p_value = calc_cwas(
+        X, Y, np.array([0, 1, 2], dtype=int), 1000, [0])
 
     assert np.isclose(p_value.mean(), 1.0, rtol=0.1)
