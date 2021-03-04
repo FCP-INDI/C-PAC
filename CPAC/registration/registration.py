543d29a2763003a684a9ecc4cc47fdd6e1308e15
Jon Clucas <jon.clucas@childmind.org>
2021-03-04 10:48:08 -0500

    :zap: Increase memory estimates by 0.5GB on overrunning nodes
    As seen in https://github.com/FCP-INDI/C-PAC/pull/1457#issue-584873393

--- CPAC/registration/registration.py
+++ CPAC/registration/registration.py
@@ -2272,7 +2272,8 @@ def warp_timeseries_to_T1template(wf, cfg, strat_pool, pipe_num, opt=None):
 
     apply_xfm = apply_transform(f'warp_ts_to_T1template_{pipe_num}', reg_tool,
                                 time_series=True, num_cpus=num_cpus,
-                                num_ants_cores=num_ants_cores)
+                                num_ants_cores=num_ants_cores,
+                                mem_gb=2.5)
 
     if reg_tool == 'ants':
         apply_xfm.inputs.inputspec.interpolation = cfg.registration_workflows[
@@ -2487,9 +2488,10 @@ def warp_timeseries_to_EPItemplate(wf, cfg, strat_pool, pipe_num, opt=None):
 
     num_ants_cores = cfg.pipeline_setup['system_config']['num_ants_threads']
 
-    apply_xfm = apply_transform('warp_ts_to_EPItemplate', reg_tool,
+    apply_xfm = apply_transform(f'warp_ts_to_EPItemplate_{pipe_num}', reg_tool,
                                 time_series=True, num_cpus=num_cpus,
-                                num_ants_cores=num_ants_cores)
+                                num_ants_cores=num_ants_cores,
+                                mem_gb=2.5)
 
     if reg_tool == 'ants':
         apply_xfm.inputs.inputspec.interpolation = cfg.registration_workflows[
