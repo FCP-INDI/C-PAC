4feb03660cd6b591efe6241c8c0e1f65bf194d6d
Jon Clucas <jon.clucas@childmind.org>
2021-04-03 09:03:39 -0400

    :loud_sound: Echo coverage location

--- dev/circleci_data/test_in_image.sh
+++ dev/circleci_data/test_in_image.sh
@@ -3,3 +3,5 @@ pip install -r /code/dev/circleci_data/requirements.txt
 
 # run test with coverage as module
 python -m coverage run -m pytest --ignore-glob=*test_install.py --junitxml=test-results/junit.xml --doctest-modules dev/circleci_data /code/CPAC
+
+echo "coverage saved to ${COVERAGE_FILE}"
