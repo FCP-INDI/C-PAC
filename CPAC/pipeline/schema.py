82ed099082ab851f262d28d632dbf241104757a5
sgiavasis <sgiava77@gmail.com>
2021-04-20 19:54:52 +0000

    Introduced ability to turn some basic node blocks off, and fixed an output directory ingressing bug.

--- CPAC/pipeline/schema.py
+++ CPAC/pipeline/schema.py
@@ -253,6 +253,7 @@ schema = Schema({
                 'be populated if \'run\' is not set to Off',
         ),
         'brain_extraction': {
+            'run': bool,
             'using': [In(valid_options['brain_extraction']['using'])],
             'AFNI-3dSkullStrip': {
                 'mask_vol': bool,
@@ -594,6 +595,7 @@ schema = Schema({
         },
         '2-nuisance_regression': {
             'run': forkable,
+            'create_regressors': bool,
             'Regressors': Maybe([Schema({
                 'Name': Required(str),
                 'Censor': {
