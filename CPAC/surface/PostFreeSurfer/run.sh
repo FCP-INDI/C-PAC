924f8f8f538a1da204d9c2cd2ae719b9f14cf56f
Xinhui Li <sucyli1017@gmail.com>
2021-07-19 15:03:33 -0400

    fix fMRISurface arg bugs, clean up code

--- CPAC/surface/PostFreeSurfer/run.sh
+++ CPAC/surface/PostFreeSurfer/run.sh
@@ -1,5 +1,4 @@
 # Input Variables
-
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --post_freesurfer_folder) StudyFolder="$2"; shift ;;
@@ -16,54 +15,17 @@ while [[ "$#" -gt 0 ]]; do
 		--inverse_atlas_transform) InverseAtlasTransformCPAC="$2"; shift ;;
 		--atlas_t1w) AtlasSpaceT1wImageCPAC="$2"; shift ;;
 		--t1w_restore) T1wRestoreImageCPAC="$2"; shift ;;
-        # *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
 
-# StudyFolder=`opts_GetOpt1 "--post_freesurfer_folder" $@`
-# Subject=`opts_GetOpt1 "--subject" $@`
-# SurfaceAtlasDIR=`opts_GetOpt1 "--surfatlasdir" $@`
-# GrayordinatesSpaceDIR=`opts_GetOpt1 "--grayordinatesdir" $@`
-# GrayordinatesResolutions=`opts_GetOpt1 "--grayordinatesres" $@`
-# HighResMesh=`opts_GetOpt1 "--hiresmesh" $@`
-# LowResMeshes=`opts_GetOpt1 "--lowresmesh" $@`
-# SubcorticalGrayLabels=`opts_GetOpt1 "--subcortgraylabels" $@`
-# FreeSurferLabels=`opts_GetOpt1 "--freesurferlabels" $@`
-# ReferenceMyelinMaps=`opts_GetOpt1 "--refmyelinmaps" $@`
-# CorrectionSigma=`opts_GetOpt1 "--mcsigma" $@`
 RegName=MSMSulc
-# InflateExtraScale=`opts_GetOpt1 "--inflatescale" $@`
 useT2=false
-
-# Extra arguments for ANTs based Atlas Registration
-# useStudyTemplate=`opts_GetOpt1 "--useStudyTemplate" $@`
-# StudyTemplate=`opts_GetOpt1 "--studytemplate" $@`
-# StudyTemplateBrain=`opts_GetOpt1 "--studytemplatebrain" $@`
-# T1wTemplate=`opts_GetOpt1 "--t1template" $@`
-# T1wTemplateBrain=`opts_GetOpt1 "--t1templatebrain" $@`
-# T1wTemplate2mm=`opts_GetOpt1 "--t1template2mm" $@`
-# T1wTemplate2mmBrain=`opts_GetOpt1 "--t1template2mmbrain" $@`
-# T2wTemplate=`opts_GetOpt1 "--t2template" $@`
-# T2wTemplateBrain=`opts_GetOpt1 "--t2templatebrain" $@`
-# T2wTemplate2mm=`opts_GetOpt1 "--t2template2mm" $@`
-# TemplateMask=`opts_GetOpt1 "--templatemask" $@`
-# Template2mmMask=`opts_GetOpt1 "--template2mmmask" $@`
-
-#################### ABIDE FIX ########################
-# Reference2mm=`opts_GetOpt1 "--reference2mm" $@`
-# Reference2mmMask=`opts_GetOpt1 "--reference2mmmask" $@`
-# FNIRTConfig=`opts_GetOpt1 "--config" $@`
-#######################################################
-
-# log_Msg "RegName: ${RegName}"
 
 # default parameters
 CorrectionSigma=$(echo "sqrt ( 200 )" | bc -l)
 # RegName=FS
 InflateExtraScale=1
-
-# PipelineScripts=${HCPPIPEDIR_PostFS}
 
 #Naming Conventions
 T1wImage="T1w_acpc_dc"
@@ -72,21 +34,11 @@ T2wFolder="T2w" #Location of T1w images
 T2wImage="T2w_acpc_dc" 
 AtlasSpaceFolder="MNINonLinear"
 NativeFolder="Native"
-# FreeSurferFolder=`opts_GetOpt1 "--freesurfer_folder" $@`
 FreeSurferInput="T1w_acpc_dc_restore_1mm"
-# AtlasTransformCPAC=`opts_GetOpt1 "--atlas_transform" $@`
-# /data3/cnl/xli/reproducibility/out/cpac_abcd/output/cpac_cpac_abcd-options/sub-0025427a_ses-1/anat/sub-0025427a_ses-1_from-T1w_to-template_mode-image_xfm.nii.gz # from-T1w_to-template_mode-image_xfm
 AtlasTransform="acpc_dc2standard"
-# InverseAtlasTransformCPAC=`opts_GetOpt1 "--inverse_atlas_transform" $@`
-# /data3/cnl/cpac1.8.1/abcd_scout/output/cpac_cpac_abcd-options/sub-0025427_ses-1/anat/sub-0025427_ses-1_from-template_to-T1w_mode-image_xfm.nii.gz
-# InverseAtlasTransformABCD=/data3/cnl/fmriprep/Lei_working/CPAC_XCP/ABCD/ABCD_Testing_With_Intermediate/sub-0025427/ses-1/files/MNINonLinear/xfms/standard2acpc_dc.nii.gz
 InverseAtlasTransform="standard2acpc_dc"
-# AtlasSpaceT1wImageCPAC=`opts_GetOpt1 "--atlas_t1w" $@`
-# /data3/cnl/xli/reproducibility/out/cpac_abcd/working/cpac_sub-0025427a_ses-1/FSL-ABCD_T1_to_template_81/sub-0025427a_run-1_T1w_resample_noise_corrected_corrected_warp_maths_restore_maths_warp.nii.gz
 AtlasSpaceT1wImage="T1w_restore"
 AtlasSpaceT2wImage="T2w_restore"
-# T1wRestoreImageCPAC=`opts_GetOpt1 "--t1w_restore" $@`
-# /data3/cnl/xli/reproducibility/out/cpac_abcd/working/cpac_sub-0025427a_ses-1/fast_bias_field_correction_52/get_anat_restore/sub-0025427a_run-1_T1w_resample_noise_corrected_corrected_warp_maths_restore_maths.nii.gz # DCAN origin file: "T1w_acpc_dc_restore"; CPAC RP item: "desc-restore-brain_T1w"
 T1wRestoreImage="T1w_acpc_dc_restore"
 T2wRestoreImage="T2w_acpc_dc_restore"
 OrginalT1wImage="T1w"
@@ -120,8 +72,6 @@ Jacobian="NonlinearRegJacobians.nii.gz"
 T1wFolder="$StudyFolder"/"$T1wFolder" 
 T2wFolder="$StudyFolder"/"$T2wFolder" 
 AtlasSpaceFolder="$StudyFolder"/"$AtlasSpaceFolder"
-# FreeSurferFolder="$T1wFolder"/"$FreeSurferFolder"
-# FreeSurferFolder=/data3/cnl/xli/reproducibility/out/cpac_abcd/working/cpac_sub-0025427a_ses-1/anat_preproc_freesurfer_52/anat_freesurfer/recon_all
 AtlasTransform="$AtlasSpaceFolder"/xfms/"$AtlasTransform"
 InverseAtlasTransform="$AtlasSpaceFolder"/xfms/"$InverseAtlasTransform"
 
--- CPAC/surface/fMRISurface/run.sh
+++ CPAC/surface/fMRISurface/run.sh
@@ -1,26 +1,17 @@
+# Input Variables
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         --post_freesurfer_folder) Path="$2"; shift ;;
         --subject) Subject="$2"; shift ;;
-		--lowresmesh) LowResMeshes="$2"; shift ;;
+		--lowresmesh) LowResMesh="$2"; shift ;;
 		--fmrires) FinalfMRIResolution="$2"; shift ;;
 		--smoothingFWHM) SmoothingFWHM="$2"; shift ;;
 		--grayordinatesres) GrayordinatesResolution="$2"; shift ;;
 		--fmri) AtlasSpacefMRI="$2"; shift ;;
 		--scout) Scout="$2"; shift ;;
-        # *) echo "Unknown parameter passed: $1"; exit 1 ;;
     esac
     shift
 done
-
-# Path=`opts_GetOpt1 "--post_freesurfer_folder" $@`
-# Subject=`opts_GetOpt1 "--subject" $@`
-# LowResMesh=`opts_GetOpt1 "--lowresmesh" $@`
-# FinalfMRIResolution=`opts_GetOpt1 "--fmrires" $@`
-# SmoothingFWHM=`opts_GetOpt1 "--smoothingFWHM" $@`
-# GrayordinatesResolution=`opts_GetOpt1 "--grayordinatesres" $@`
-# AtlasSpacefMRI=`opts_GetOpt1 "--fmri" $@` # func to standard
-# Scout=`opts_GetOpt1 "--scout" $@`
 
 NameOffMRI=task-rest01
 RegName=MSMSulc
@@ -34,7 +25,7 @@ OutputAtlasDenseTimeseries="${NameOffMRI}_Atlas"
 
 AtlasSpaceFolder="$Path"/"$AtlasSpaceFolder"
 T1wFolder="$Path"/"$T1wFolder"
-ResultsFolder="$AtlasSpaceFolder"/"$ResultsFolder"/"$NameOffMRI" # we don't have this folder so far, make one
+ResultsFolder="$AtlasSpaceFolder"/"$ResultsFolder"/"$NameOffMRI"
 ROIFolder="$AtlasSpaceFolder"/"$ROIFolder"
 
 if [ ! -e "$ResultsFolder" ] ; then
@@ -55,16 +46,16 @@ fi
 echo "Make fMRI Ribbon"
 echo "mkdir -p ${ResultsFolder}/RibbonVolumeToSurfaceMapping"
 mkdir -p "$ResultsFolder"/RibbonVolumeToSurfaceMapping
-bash RibbonVolumeToSurfaceMapping.sh "$ResultsFolder"/RibbonVolumeToSurfaceMapping "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$DownSampleFolder" "$LowResMesh" "$AtlasSpaceFolder"/"$NativeFolder" "${RegName}"
+bash /code/CPAC/surface/fMRISurface/RibbonVolumeToSurfaceMapping.sh "$ResultsFolder"/RibbonVolumeToSurfaceMapping "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$DownSampleFolder" "$LowResMesh" "$AtlasSpaceFolder"/"$NativeFolder" "${RegName}"
 
 #Surface Smoothing
 echo "Surface Smoothing"
-bash SurfaceSmoothing.sh "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$DownSampleFolder" "$LowResMesh" "$SmoothingFWHM"
+bash /code/CPAC/surface/fMRISurface/SurfaceSmoothing.sh "$ResultsFolder"/"$NameOffMRI" "$Subject" "$AtlasSpaceFolder"/"$DownSampleFolder" "$LowResMesh" "$SmoothingFWHM"
 
 #Subcortical Processing
 echo "Subcortical Processing"
-bash SubcorticalProcessing.sh "$AtlasSpaceFolder" "$ROIFolder" "$FinalfMRIResolution" "$ResultsFolder" "$NameOffMRI" "$SmoothingFWHM" "$GrayordinatesResolution"
+bash /code/CPAC/surface/fMRISurface/SubcorticalProcessing.sh "$AtlasSpaceFolder" "$ROIFolder" "$FinalfMRIResolution" "$ResultsFolder" "$NameOffMRI" "$SmoothingFWHM" "$GrayordinatesResolution"
 
 #Generation of Dense Timeseries
 echo "Generation of Dense Timeseries"
-bash CreateDenseTimeseries.sh "$AtlasSpaceFolder"/"$DownSampleFolder" "$Subject" "$LowResMesh" "$ResultsFolder"/"$NameOffMRI" "$SmoothingFWHM" "$ROIFolder" "$ResultsFolder"/"$OutputAtlasDenseTimeseries" "$GrayordinatesResolution"
+bash /code/CPAC/surface/fMRISurface/CreateDenseTimeseries.sh "$AtlasSpaceFolder"/"$DownSampleFolder" "$Subject" "$LowResMesh" "$ResultsFolder"/"$NameOffMRI" "$SmoothingFWHM" "$ROIFolder" "$ResultsFolder"/"$OutputAtlasDenseTimeseries" "$GrayordinatesResolution"
