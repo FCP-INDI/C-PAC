c76e667622949d90281a55abcfcfdff3ada309ca
Jon Clucas <jon.clucas@childmind.org>
2021-09-16 13:17:32 -0400

    :coffin: Remove unused `run_node`

--- CPAC/pipeline/plugins/__init__.py
+++ CPAC/pipeline/plugins/__init__.py
@@ -1,6 +1,5 @@
 """Import Nipype's pipeline plugins and selectively override"""
 from nipype.pipeline.plugins import *  # noqa F401,F403
-from .base import run_node  # noqa F401
 # Override LegacyMultiProc
 from .legacymultiproc import LegacyMultiProcPlugin  # noqa F401
 # Override MultiProc
