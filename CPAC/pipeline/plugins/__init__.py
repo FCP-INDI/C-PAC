d6fc4ff1418059842589a6b61bf1e903cdf9773d
Jon Clucas <jon.clucas@childmind.org>
2021-10-26 15:39:56 -0400

    fixup! :truck: Move plugins into nipype_pipeline_engine

--- CPAC/pipeline/plugins/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Import Nipype's pipeline plugins and selectively override"""
-from nipype.pipeline.plugins import *  # noqa F401,F403
-# Override LegacyMultiProc
-from .legacymultiproc import LegacyMultiProcPlugin  # noqa F401
-# Override MultiProc
-from .multiproc import MultiProcPlugin  # noqa F401
