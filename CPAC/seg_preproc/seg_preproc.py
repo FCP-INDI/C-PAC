e72fd5b1ae0ec0e52d7e17a30c0637ac8121c85c
amygutierrez <amygutierrezbme@gmail.com>
2021-10-27 14:22:56 -0400

    removed xfm = None

--- CPAC/seg_preproc/seg_preproc.py
+++ CPAC/seg_preproc/seg_preproc.py
@@ -601,7 +601,7 @@ def tissue_seg_fsl_fast(wf, cfg, strat_pool, pipe_num, opt=None):
 
     use_priors = cfg['segmentation']['tissue_segmentation'][
         'FSL-FAST']['use_priors']['run']
-    xfm = None
+
     if use_priors: 
         long = ''
         xfm = 'from-template_to-T1w_mode-image_desc-linear_xfm'
