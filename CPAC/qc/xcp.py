c6016fc9829cfd212ad569ea0df97db055ec4aa5
Jon Clucas <jon.clucas@childmind.org>
2021-11-11 11:30:24 -0500

    :construction:

--- CPAC/qc/xcp.py
+++ CPAC/qc/xcp.py
@@ -16,37 +16,38 @@ space : str
     space label :cite:`cite-BIDS21`
 meanFD : float
     mean Jenkinson framewise displacement :cite:`cite-Jenk02` :func:`CPAC.generate_motion_statistics.calculate_FD_J`
-relMeansRMSMotion
-
-relMaxRMSMotion
-
-meanDVInit
-
-meanDVFinal
-
-nVolCensored
-
-nVolsRemoved
-
-motionDVCorrInit
-
-motionDVCorrFinal
-
-coregDice
-
-coregJaccard
-
-coregCrossCorr
-
-coregCoverag
-
-normDice
-
-normJaccard
-
-normCrossCorr
-
-normCoverage
+relMeansRMSMotion : float
+    # TODO
+relMaxRMSMotion : float
+    # TODO
+meanDVInit : float
+    # TODO
+meanDVFinal : float
+    # TODO
+nVolCensored : int
+    # TODO
+nVolsRemoved : int
+    # TODO
+motionDVCorrInit : float
+    # TODO
+motionDVCorrFinal : float
+    # TODO
+coregDice : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
+coregJaccard : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
+coregCrossCorr : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
+coregCoverag : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
+normDice : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
+normJaccard : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
+normCrossCorr : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
+normCoverage : float
+    :cite:`cite-Ciri19` :cite:`cite-Penn19`
 
 .. rubric References
 
@@ -66,6 +67,14 @@ from CPAC.pipeline import nipype_pipeline_engine as pe
 from CPAC.utils.interfaces.function import Function
 
 
+# class OverlapInterface(IdentityInterface):
+#     """XCP QC interface for overlap measures :cite:`cite-Ciri19` :cite:`cite-Penn19`."""  # noqa E501  # pylint: disable=line-too-long
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args,
+#                          fields=['Dice', 'Jaccard', 'CrossCorr', 'Coverage'],
+#                          **kwargs)
+
+
 def generate_desc_qc(original, final):
     """Function to generate an RBC-style QC CSV
 
@@ -77,6 +86,14 @@ def generate_desc_qc(original, final):
     final : str
         path to final image
 
+    coreg : nipype.interfaces.base.specs.DynamicTraitedSpec
+        Output of OverlapInterface (with attributes Dice, Jaccard,
+        CrossCorr, and Coverage)
+
+    norm : nipype.interfaces.base.specs.DynamicTraitedSpec
+        Output of OverlapInterface (with attributes Dice, Jaccard,
+        CrossCorr, and Coverage)
+
     Returns
     -------
     str
@@ -86,9 +103,14 @@ def generate_desc_qc(original, final):
         'sub,ses,task,run,desc,space,meanFD,relMeansRMSMotion,'
         'relMaxRMSMotion,meanDVInit,meanDVFinal,nVolCensored,nVolsRemoved,'
         'motionDVCorrInit,motionDVCorrFinal,coregDice,coregJaccard,'
-        'coregCrossCorr,coregCoverag,normDice,normJaccard,normCrossCorr,'
+        'coregCrossCorr,coregCoverage,normDice,normJaccard,normCrossCorr,'
         'normCoverage'.split(',')
     )
+
+    images = {
+        'original': nb.load(original),
+        'final': nb.load(final)
+    }
 
     # `sub` through `space`
     final_filename = final.split('/')[-1]
@@ -102,7 +124,7 @@ def generate_desc_qc(original, final):
         from_bids['space'] = ['native']
 
     # `nVolCensored` & `nVolsRemoved`
-    if nb.load(original).shape == nb.load(final).shape:
+    if images['original'].shape == images['final'].shape:
         shape_diff = 0
     else:
         shape_diff = 'qc log not yet implemented'  # TODO
@@ -142,11 +164,33 @@ def generate_desc_qc(original, final):
     # `meanDVinit` & `meanDVFinal`
     # ?
 
+    # Overlap
+    images = {variable: image.get_fdata().ravel() for
+              variable, image in images.items()}
+    intersect = images['original'] * images['final']
+    vols = {variable: np.sum(image) for variable, image in images.items()}
+    vol_intersect = np.sum(intersect)
+    vol_sum = sum(vols.values())
+    vol_union = vol_sum - vol_intersect
+    overlap_params = {
+        'coregDice': 2 * vol_intersect / vol_sum,
+        'coregJaccard': vol_intersect / vol_union,
+        'coregCrossCorr': np.corrcoef(
+            images['original'],
+            images['final'])[0, 1],
+        'coregCoverage': vol_intersect / min(vols.values()),
+        'normDice': 'N/A: native space',
+        'normJaccard': 'N/A: native space',
+        'normCrossCorr': 'N/A: native space',
+        'normCoverage': 'N/A: native space'
+    }
+
     qc_dict = {
         **from_bids,
         **power_params,
         **rms_params,
         **shape_params,
+        **overlap_params
     }
     df = pd.DataFrame(qc_dict, columns=columns)
     df.to_csv(qc_filepath, sep='\t', index=False)
