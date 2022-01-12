44589376c110292fcaf8bece32b177e801fad091
Jon Clucas <jon.clucas@childmind.org>
2022-01-12 13:15:08 -0500

    :rewind: Revert 067eca5
    [skip ci]

--- CPAC/qc/xcp.py
+++ CPAC/qc/xcp.py
@@ -60,14 +60,11 @@ import pandas as pd
 from CPAC.generate_motion_statistics.generate_motion_statistics import \
     motion_power_statistics
 from CPAC.pipeline import nipype_pipeline_engine as pe
-from CPAC.pipeline.engine import get_node_block_config
 from CPAC.utils.interfaces.function import Function
 from CPAC.utils.utils import check_prov_for_motion_tool
 
 motion_params = ['movement-parameters', 'dvars',
                  'framewise-displacement-jenkinson']
-raw_resources = ['bold', 'subject', 'scan', 'T1w',
-                 'T1w-brain-template-funcreg']
 
 
 def calculate_overlap(image_pair):
@@ -446,10 +443,6 @@ def qc_xcp_native(wf, cfg, strat_pool, pipe_num, opt=None):
                 'coordinate-transformation')],
      'outputs': ['desc-xcp_quality']}
     """
-    if not strat_pool.check_rpool([resource for resource in raw_resources if
-                                  resource in get_node_block_config(
-                                      qc_xcp_native, 'inputs')[0]]):
-        return wf, {}
     space = 'native'
     qc_file, original, final, t1w_bold = _prep_qc_xcp(strat_pool, pipe_num,
                                                       space)
@@ -474,10 +467,6 @@ def qc_xcp_skullstripped(wf, cfg, strat_pool, pipe_num, opt=None):
                 'space-bold_desc-brain_mask')],
      'outputs': ['desc-xcp_quality']}
     """
-    if not strat_pool.check_rpool([resource for resource in raw_resources if
-                                  resource in get_node_block_config(
-                                      qc_xcp_skullstripped, 'inputs')[0]]):
-        return wf, {}
     return qc_xcp_native(wf, cfg, strat_pool, pipe_num, opt)
 
 
@@ -495,10 +484,6 @@ def qc_xcp_template(wf, cfg, strat_pool, pipe_num, opt=None):
                 'space-template_desc-bold_mask')],
      'outputs': ['space-template_desc-xcp_quality']}
     """
-    if not strat_pool.check_rpool([resource for resource in raw_resources if
-                                  resource in get_node_block_config(
-                                      qc_xcp_template, 'inputs')[0]]):
-        return wf, {}
     space = 'template'
     qc_file, original, final, t1w_bold = _prep_qc_xcp(strat_pool, pipe_num,
                                                       space)
