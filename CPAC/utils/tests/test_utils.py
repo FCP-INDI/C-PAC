6c94bb1c92a08b07c1e78bea925939c197444bb8
Jon Clucas <jon.clucas@childmind.org>
2022-05-26 11:30:12 -0400

    :white_check_mark: Add test for check_config_resources function

--- CPAC/utils/tests/test_utils.py
+++ CPAC/utils/tests/test_utils.py
@@ -1,5 +1,10 @@
 """Tests for C-PAC utilities"""
-from CPAC.utils.utils import check_system_deps, try_fetch_parameter
+import multiprocessing
+from unittest import mock
+import pytest
+from CPAC.utils.configuration import Configuration
+from CPAC.utils.utils import check_config_resources, check_system_deps, \
+                             try_fetch_parameter
 
 scan_params_bids = {
     "RepetitionTime": 2.0,
@@ -14,6 +19,17 @@ scan_params_cpac = {
     'first_tr': '',
     'last_tr': '',
 }
+
+
+def test_check_config_resources():
+    """Test check_config_resources function."""
+    with mock.patch.object(multiprocessing, 'cpu_count', return_value=2), \
+         pytest.raises(SystemError) as system_error:
+        check_config_resources(Configuration({'pipeline_setup': {
+            'system_config': {'max_cores_per_participant': 10}}}))
+    error_string = str(system_error.value)
+    assert 'threads running in parallel (10)' in error_string
+    assert 'threads available (2)' in error_string
 
 
 def test_function():
