073b97df3912619433ff43944694b4bf1a4b4ad0
Jon Clucas <jon.clucas@childmind.org>
2022-06-10 11:46:30 -0400

    :memo: Restore 'benchmark-FNIRT' to official avaialable preconfig list
    Ref https://github.com/FCP-INDI/C-PAC/pull/1734#issuecomment-1151621877
    
    Co-authored-by: Steve Giavasis <sgiava77@gmail.com>

--- CPAC/pipeline/__init__.py
+++ CPAC/pipeline/__init__.py
@@ -8,7 +8,7 @@ ALL_PIPELINE_CONFIGS = [x.split('_')[2].replace('.yml', '') for
                         x in ALL_PIPELINE_CONFIGS if 'pipeline_config' in x]
 ALL_PIPELINE_CONFIGS.sort()
 AVAILABLE_PIPELINE_CONFIGS = [preconfig for preconfig in ALL_PIPELINE_CONFIGS
-                              if not preconfig.startswith('benchmark-') and
+                              if preconfig != 'benchmark-ANTS' and
                               not preconfig.startswith('regtest-')]
 
 __all__ = ['ALL_PIPELINE_CONFIGS', 'AVAILABLE_PIPELINE_CONFIGS']
