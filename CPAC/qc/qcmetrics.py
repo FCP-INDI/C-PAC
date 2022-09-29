8cc3f5aa638e9d2ece84def46a60f7fe76ad432f
Jon Clucas <jon.clucas@childmind.org>
2022-09-29 11:35:33 -0400

    :recycle: Modularize mask parameter checking

--- CPAC/qc/qcmetrics.py
+++ CPAC/qc/qcmetrics.py
@@ -7,16 +7,29 @@ import nibabel as nb
 import numpy as np
 
 
-def regisQ(bold2t1w_mask, t1w_mask, bold2template_mask, template_mask):
-    reg_qc = {'coregDice': [dc(bold2t1w_mask, t1w_mask)],
-              'coregJaccard': [jc(bold2t1w_mask, t1w_mask)],
-              'coregCrossCorr': [crosscorr(bold2t1w_mask, t1w_mask)],
-              'coregCoverage': [coverage(bold2t1w_mask, t1w_mask)],
-              'normDice': [dc(bold2template_mask, template_mask)],
-              'normJaccard': [jc(bold2template_mask, template_mask)],
-              'normCrossCorr': [crosscorr(bold2template_mask, template_mask)],
-              'normCoverage': [coverage(bold2template_mask, template_mask)]}
-    return reg_qc
+def coverage(input1, input2):
+    """Estimate the coverage between two masks."""
+    input1 = nb.load(input1).get_fdata()
+    input2 = nb.load(input2).get_fdata()
+    input1 = np.atleast_1d(input1.astype(np.bool))
+    input2 = np.atleast_1d(input2.astype(np.bool))
+    intsec = np.count_nonzero(input1 & input2)
+    if np.sum(input1) > np.sum(input2):
+        smallv = np.sum(input2)
+    else:
+        smallv = np.sum(input1)
+    cov = float(intsec)/float(smallv)
+    return cov
+
+
+def crosscorr(input1, input2):
+    r"""cross correlation: compute cross correction bewteen input masks"""
+    input1 = nb.load(input1).get_fdata()
+    input2 = nb.load(input2).get_fdata()
+    input1 = np.atleast_1d(input1.astype(np.bool)).flatten()
+    input2 = np.atleast_1d(input2.astype(np.bool)).flatten()
+    cc = np.corrcoef(input1, input2)[0][1]
+    return cc
 
 
 def dc(input1, input2):
@@ -106,26 +119,57 @@ def jc(input1, input2):
     return jc
 
 
-def crosscorr(input1, input2):
-    r"""cross correlation: compute cross correction bewteen input masks"""
-    input1 = nb.load(input1).get_fdata()
-    input2 = nb.load(input2).get_fdata()
-    input1 = np.atleast_1d(input1.astype(np.bool)).flatten()
-    input2 = np.atleast_1d(input2.astype(np.bool)).flatten()
-    cc = np.corrcoef(input1, input2)[0][1]
-    return cc
+def _prefix_regqc_keys(qc_dict: dict, prefix: str) -> str:
+    """Prepend string to each key in a qc dict
+
+    Parameters
+    ----------
+    qc_dict : dict
+        output of ``qc_masks``
+
+    prefix : str
+        string to prepend
+
+    Returns
+    -------
+    dict
+    """
+    return {f'{prefix}{_key}': _value for _key, _value in qc_dict.items()}
 
 
-def coverage(input1, input2):
-    """Estimate the coverage between two masks."""
-    input1 = nb.load(input1).get_fdata()
-    input2 = nb.load(input2).get_fdata()
-    input1 = np.atleast_1d(input1.astype(np.bool))
-    input2 = np.atleast_1d(input2.astype(np.bool))
-    intsec = np.count_nonzero(input1 & input2)
-    if np.sum(input1) > np.sum(input2):
-        smallv = np.sum(input2)
-    else:
-        smallv = np.sum(input1)
-    cov = float(intsec)/float(smallv)
-    return cov
+def qc_masks(registered_mask: str, native_mask: str) -> dict:
+    """Return QC measures for coregistration
+
+    Parameters
+    ----------
+    registered_mask : str
+        path to registered mask
+
+    native_mask : str
+        path to native-space mask
+
+    Returns
+    -------
+    dict
+    """
+    return {'Dice': [dc(registered_mask, native_mask)],
+            'Jaccard': [jc(registered_mask, native_mask)],
+            'CrossCorr': [crosscorr(registered_mask, native_mask)],
+            'Coverage': [coverage(registered_mask, native_mask)]}
+
+
+def regisQ(bold2t1w_mask: str, t1w_mask: str, bold2template_mask: str,
+           template_mask: str) -> dict:
+    """Collect coregistration QC measures
+
+    Parameters
+    ----------
+    bold2t1w_mask, t1w_mask, bold2template_mask, template_mask : str
+
+    Returns
+    -------
+    dict
+    """
+    return {**_prefix_regqc_keys(qc_masks(bold2t1w_mask, t1w_mask), 'coreg'),
+            **_prefix_regqc_keys(qc_masks(bold2template_mask, template_mask),
+                                 'norm')}
