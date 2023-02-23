7a84318ff9fe6a471ab267d0595efff60f4ed4b8
Florian Rupprecht <floruppr@gmail.com>
2023-02-23 14:20:30 -0500

    Initial decorator prototype

---
+++ CPAC/pipeline/nodeblock.py
@@ -0,0 +1,38 @@
+from typing import Callable, List, Union, Optional
+
+
+class NodeBlock(object):
+    def __init__(
+            self,
+            func: Callable,
+            name: Optional[str] = None,
+            config: Optional[List[str]] = None,
+            switch: Optional[Union[List[str], List[List[str]]]] = None,
+            option_key: Optional[Union[str, List[str]]] = None,
+            option_val: Optional[Union[str, List[str]]] = None,
+            inputs: Optional[List[Union[str, list, tuple]]] = None,
+            outputs: Optional[List[str]] = None
+    ) -> None:
+        self.func = func
+        self.name: Optional[str] = name
+        self.config: Optional[List[str]] = config
+        self.switch: Optional[Union[List[str], List[List[str]]]] = switch
+        self.option_key: Optional[Union[str, List[str]]] = option_key
+        self.option_val: Optional[Union[str, List[str]]] = option_val
+        self.inputs: Optional[List[Union[str, list, tuple]]] = inputs
+        self.outputs: Optional[List[str]] = outputs
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+
+def nodeblock(
+        name: Optional[str] = None,
+        config: Optional[List[str]] = None,
+        switch: Optional[Union[List[str], List[List[str]]]] = None,
+        option_key: Optional[Union[str, List[str]]] = None,
+        option_val: Optional[Union[str, List[str]]] = None,
+        inputs: Optional[List[Union[str, list, tuple]]] = None,
+        outputs: Optional[List[str]] = None
+):
+    return lambda func: NodeBlock(func, name, config, switch, option_key, option_val, inputs, outputs)
