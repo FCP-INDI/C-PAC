34996845eb08d4e729a4eb5f0bb065a353b645a1
Jon Clucas <jon.clucas@childmind.org>
2023-03-22 17:17:24 -0400

    :pencil2: Remove extra copyright notice

--- CPAC/utils/monitoring/custom_logging.py
+++ CPAC/utils/monitoring/custom_logging.py
@@ -2,8 +2,6 @@
 
 # This file is part of C-PAC.
 
-# Copyright (C) 2022-2023  C-PAC Developers
-
 # C-PAC is free software: you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or (at your
