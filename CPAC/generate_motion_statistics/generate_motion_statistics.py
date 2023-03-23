8ac5c3c8df697cd062d4bcad449b312b446c19c8
Jon Clucas <jon.clucas@childmind.org>
2023-03-23 17:04:26 -0400

    :recycle: Adjust filtered connections

--- CPAC/generate_motion_statistics/generate_motion_statistics.py
+++ CPAC/generate_motion_statistics/generate_motion_statistics.py
@@ -1,3 +1,20 @@
+# Copyright (C) 2012-2023  C-PAC Developers
+
+# This file is part of C-PAC.
+
+# C-PAC is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# C-PAC is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with C-PAC. If not, see <https://www.gnu.org/licenses/>.
+"""Functions to calculate motion statistics"""
 import os
 import numpy as np
 import nibabel as nb
@@ -7,6 +24,7 @@ from CPAC.utils.interfaces.function import Function
 
 from nipype.interfaces.afni.base import (AFNICommand, AFNICommandInputSpec)
 from nipype.interfaces.base import (TraitedSpec, traits, isdefined, File)
+
 
 def motion_power_statistics(name='motion_stats',
                             motion_correct_tool='3dvolreg'):
