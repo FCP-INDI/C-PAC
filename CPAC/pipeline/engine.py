c508625c783c5f2f6eacaa252a153d4cb60caf7f
Elizabeth Kenneally <113037677+e-kenneally@users.noreply.github.com>
2023-04-11 11:42:52 -0400

    :arrow_right: Ingress transform from freesurfer_dir

--- CPAC/pipeline/engine.py
+++ CPAC/pipeline/engine.py
@@ -1721,6 +1721,7 @@ def ingress_raw_anat_data(wf, rpool, cfg, data_paths, unique_id, part_id,
             'pipeline-fs_hemi-R_desc-surfaceMap_volume': 'surf/rh.volume',
             'pipeline-fs_hemi-L_desc-surfaceMesh_white': 'surf/lh.white',
             'pipeline-fs_hemi-R_desc-surfaceMesh_white': 'surf/rh.white',
+            'pipeline-fs_xfm': 'mri/transforms/talairach.xfm'
         }
         
         for key, outfile in recon_outs.items():
