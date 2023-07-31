4deabb49c32a80fa31dccc32488785a61c67929d
Jon Clucas <jon.clucas@childmind.org>
2023-07-31 16:58:19 -0400

    :bug: Handle `poll() == None` in version check

--- CPAC/utils/versioning/dependencies.py
+++ CPAC/utils/versioning/dependencies.py
@@ -53,7 +53,8 @@ def cli_version(command, dependency=None, in_result=True, delimiter=' ',
     """
     with Popen(command, stdout=PIPE, stderr=STDOUT, shell=True) as _command:
         _version = _command.stdout.read().decode('utf-8')
-        if int(_command.poll()) == 127:  # handle missing command
+        poll = _command.poll()
+        if poll is None or int(poll) == 127:  # handle missing command
             return {}
     if formatting is not None:
         _version = formatting(_version)
