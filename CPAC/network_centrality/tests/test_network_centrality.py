9feff2f41652b9ef4f8ed76a5ff239436216a6f5
Jon Clucas <jon.clucas@childmind.org>
2023-09-27 17:52:05 -0400

    :white_check_mark: Move expected exception

--- CPAC/network_centrality/tests/test_network_centrality.py
+++ CPAC/network_centrality/tests/test_network_centrality.py
@@ -43,6 +43,13 @@ def test_create_centrality_wf(method_option: str, weight_options: LIST[str],
     wf_name = (f'test_{method_option[0]}'
                f'{"".join([_[0] for _ in weight_options])}'
                f'{threshold_option[0]}{threshold}'.replace('.', 'p'))
+    if (method_option == 'local_functional_connectivity_density' and
+        threshold_option == 'Sparsity threshold'
+    ):
+        with pytest.raises(ValueError):
+            centrality_wf = create_centrality_wf(wf_name, method_option,
+                weight_options, threshold_option, threshold, base_dir=tmpdir)
+            return
     if method_option == 'eigenvector_centrality':
         centrality_wf = create_centrality_wf(wf_name, method_option,
             weight_options, threshold_option, threshold, memory_gb=3.0,
@@ -54,10 +61,4 @@ def test_create_centrality_wf(method_option: str, weight_options: LIST[str],
                                               'in_file.nii.gz').absolute()
     centrality_wf.inputs.inputspec.template = (_DATA_DIR /
                                                'template.nii.gz').absolute()
-    if (method_option == 'local_functional_connectivity_density' and
-        threshold_option == 'Sparsity threshold'
-    ):
-        with pytest.raises(ValueError):
-            centrality_wf.run()
-    else:
-        centrality_wf.run()
+    centrality_wf.run()
