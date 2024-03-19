6c996872404c2069e2737f6bcbb344d5abdb69c8
Jon Clucas <jon.clucas@childmind.org>
2024-03-18 22:42:53 -0400

    :page_facing_up: Add notice & statement of changes to code derived from `_tpm2roi`

--- CPAC/seg_preproc/utils.py
+++ CPAC/seg_preproc/utils.py
@@ -1,4 +1,74 @@
-# Import packages
+# Except for functions ``_erode``, ``erosion`` and ``mask_erosion``:
+# Copyright (C) 2012 - 2024 C-PAC Developers
+
+# This file is part of C-PAC.
+
+# C-PAC is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# C-PAC is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with C-PAC. If not, see <https://www.gnu.org/licenses/>.
+
+# STATEMENT OF CHANGES:
+#     Functions in this file are derived from sources licensed under the Apache-2.0 terms,
+#     and these functions have been changed.
+
+# CHANGES:
+#     * Split ``_tpm2roi`` (https://github.com/nipreps/niworkflows/blob/805cdd0/niworkflows/interfaces/utils.py#L904-L955) into ``_erode`` (common logic), ``erosion`` ("erode_out") and ``mask_erosion`` ("erode_in")
+#     * Relies on mask already being generated rather than generating the mask in any of these functions
+#     * Cast ``orig_vol`` as float in target volume ratio condition
+#     * Modified docstrings to reflect local changes
+#     * Updated style to match C-PAC codebase
+
+# ORIGINAL WORK'S ATTRIBUTION NOTICE:
+#    Copyright (c) 2016, the CRN developers team.
+#    All rights reserved.
+
+#    Redistribution and use in source and binary forms, with or without
+#    modification, are permitted provided that the following conditions are met:
+
+#    * Redistributions of source code must retain the above copyright notice, this
+#      list of conditions and the following disclaimer.
+
+#    * Redistributions in binary form must reproduce the above copyright notice,
+#      this list of conditions and the following disclaimer in the documentation
+#      and/or other materials provided with the distribution.
+
+#   * Neither the name of niworkflows nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+
+#    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+#    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+#    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+#    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Modifications copyright (C) 2019 - 2024  C-PAC Developers
 import os
 
 import numpy as np
@@ -31,7 +101,7 @@ def check_if_file_is_empty(in_file):
 
 
 def _erode(roi_mask, erosion_mm, erosion_prop):
-    """Function to perform in-common erosion steps.
+    """Perform in-common erosion steps.
 
     Parameters
     ----------
@@ -50,14 +120,18 @@ def _erode(roi_mask, erosion_mm, erosion_prop):
         Original mask image
 
     erode : bool
-        erosion_bool or erosion_mm
+        erosion_mm or erosion_prop
 
     mask_data : numpy array
         eroded mask data
     """
+    # This functionality is adapted from poldracklab/niworkflows:
+    #   https://github.com/nipreps/niworkflows/blob/805cdd0/niworkflows/interfaces/utils.py#L904-L955
+    #   https://fmriprep.readthedocs.io/
+    #   https://poldracklab.stanford.edu/
+    # We are temporarily maintaining our own copy for more granular control.
     mask_img = nib.load(roi_mask)
     mask_data = mask_img.get_fdata()
-    orig_vol = np.sum(mask_data > 0)
     erode = (erosion_mm is not None and erosion_mm > 0) or (
         erosion_prop is not None and 0 < erosion_prop < 1
     )
@@ -66,6 +140,7 @@ def _erode(roi_mask, erosion_mm, erosion_prop):
             iter_n = max(int(erosion_mm / max(mask_img.header.get_zooms())), 1)
             mask_data = nd.binary_erosion(mask_data, iterations=iter_n)
         else:
+            orig_vol = np.sum(mask_data > 0)
             while np.sum(mask_data > 0) / (orig_vol * 1.0) > erosion_prop:
                 mask_data = nd.binary_erosion(mask_data, iterations=1)
     return mask_img, erode, mask_data
@@ -209,12 +284,6 @@ def mask_erosion(
 ):
     """Returns eroded segment mask and skull-stripped brain mask.
 
-    # This functionality is adapted from poldracklab/niworkflows:
-    #   https://github.com/poldracklab/niworkflows/blob/master/niworkflows/interfaces/utils.py
-    #   https://fmriprep.readthedocs.io/
-    #   https://poldracklab.stanford.edu/
-    # We are temporarily maintaining our own copy for more granular control.
-
     Parameters
     ----------
     roi_mask : string
@@ -234,6 +303,11 @@ def mask_erosion(
     eroded_skullstrip_mask : string
         Path to eroded skull-stripped brain mask
     """
+    # This functionality is adapted from poldracklab/niworkflows:
+    #   https://github.com/nipreps/niworkflows/blob/805cdd0/niworkflows/interfaces/utils.py#L916-L935
+    #   https://fmriprep.readthedocs.io/
+    #   https://poldracklab.stanford.edu/
+    # We are temporarily maintaining our own copy for more granular control.
     roi_mask_img = nib.load(roi_mask)
     roi_mask_data = roi_mask_img.get_fdata()
     skullstrip_mask_img, erode_in, skullstrip_mask_data = _erode(
@@ -262,11 +336,6 @@ def mask_erosion(
     return output_roi_mask, eroded_skullstrip_mask
 
 
-# This functionality is adapted from poldracklab/niworkflows:
-# https://github.com/poldracklab/niworkflows/blob/master/niworkflows/interfaces/utils.py
-# https://fmriprep.readthedocs.io/
-# https://poldracklab.stanford.edu/
-# We are temporarily maintaining our own copy for more granular control.
 def erosion(roi_mask=None, erosion_mm=None, erosion_prop=None):
     """Returns eroded tissue segment mask.
 
@@ -283,6 +352,11 @@ def erosion(roi_mask=None, erosion_mm=None, erosion_prop=None):
     eroded_roi_mask : string
         Path to eroded segment mask
     """
+    # This functionality is adapted from poldracklab/niworkflows:
+    #   https://github.com/nipreps/niworkflows/blob/805cdd0/niworkflows/interfaces/utils.py#L937-L954
+    #   https://fmriprep.readthedocs.io/
+    #   https://poldracklab.stanford.edu/
+    # We are temporarily maintaining our own copy for more granular control.
     roi_mask_img, _, roi_mask_data = _erode(roi_mask, erosion_mm, erosion_prop)
 
     hdr = roi_mask_img.header
@@ -327,9 +401,7 @@ def hardcoded_antsJointLabelFusion(
 
     cmd = ["${ANTSPATH}${ANTSPATH:+/}antsJointLabelFusion.sh"]
     cmd.append(
-        " -d 3 -o ants_multiatlas_ -t {0} -x {1} -y b -c 0".format(
-            anatomical_brain, anatomical_brain_mask
-        )
+        f" -d 3 -o ants_multiatlas_ -t {anatomical_brain} -x {anatomical_brain_mask} -y b -c 0"
     )
 
     if not len(template_brain_list) == len(template_segmentation_list):
@@ -344,9 +416,7 @@ def hardcoded_antsJointLabelFusion(
     else:
         for index in range(len(template_brain_list)):
             cmd.append(
-                " -g {0} -l {1}".format(
-                    template_brain_list[index], template_segmentation_list[index]
-                )
+                f" -g {template_brain_list[index]} -l {template_segmentation_list[index]}"
             )
 
     # write out the actual command-line entry for testing/validation later
