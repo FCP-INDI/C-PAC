a2da0a42da7aed63da4199564f10653c074a8b81
Jon Clucas <jon.clucas@childmind.org>
2024-03-20 12:05:11 -0400

    :page_facing_up: Add license notice to qcmetrics

--- CPAC/qc/qcmetrics.py
+++ CPAC/qc/qcmetrics.py
@@ -1,6 +1,36 @@
+# BSD 3-Clause License
+
+# Copyright (c) 2020, Lifespan Informatics and Neuroimaging Center
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Modifications copyright (C) 2022-2024  C-PAC Developers
 """QC metrics from XCP-D v0.0.9.
 
-Ref: https://github.com/PennLINC/xcp_d/tree/0.0.9
+https://github.com/PennLINC/xcp_d/blob/0.0.9/xcp_d/utils/qcmetrics.py
 """
 
 # pylint: disable=invalid-name, redefined-outer-name
@@ -9,6 +39,7 @@ import nibabel as nib
 
 
 def regisQ(bold2t1w_mask, t1w_mask, bold2template_mask, template_mask):
+    """Collect a dictionary of registration QC metrics."""
     return {
         "coregDice": [dc(bold2t1w_mask, t1w_mask)],
         "coregJaccard": [jc(bold2t1w_mask, t1w_mask)],
@@ -23,10 +54,12 @@ def regisQ(bold2t1w_mask, t1w_mask, bold2template_mask, template_mask):
 
 def dc(input1, input2):
     r"""
-    Dice coefficient
+    Dice coefficient.
+
     Computes the Dice coefficient (also known as Sorensen index)
     between the binary objects in two images.
-    The metric is defined as.
+
+    The metric is defined as
 
     .. math::
         DC=\frac{2|A\cap B|}{|A|+|B|}
