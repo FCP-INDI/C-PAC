a52125b507bdbf53c403507dff83fd29e7473365
Jon Clucas <jon.clucas@childmind.org>
2024-03-22 15:03:08 -0400

    :page_facing_up: Add notice & statement of changes to `log_nodes_cb`

--- CPAC/utils/monitoring/monitoring.py
+++ CPAC/utils/monitoring/monitoring.py
@@ -7,6 +7,7 @@ import threading
 
 import networkx as nx
 from traits.trait_base import Undefined
+from nipype.utils.profiler import log_nodes_cb as _nipype_log_nodes_cb
 
 from CPAC.pipeline import nipype_pipeline_engine as pe
 from .custom_logging import getLogger
@@ -32,23 +33,63 @@ def log_nodes_initial(workflow):
 
 
 def log_nodes_cb(node, status):
-    """Function to record node run statistics to a log file as json
-    dictionaries.
+    # STATEMENT OF CHANGES:
+    #     This function is derived from sources licensed under the Apache-2.0 terms,
+    #     and this function has been changed.
 
-    Parameters
-    ----------
-    node : nipype.pipeline.engine.Node
-        the node being logged
-    status : string
-        acceptable values are 'start', 'end'; otherwise it is
-        considered and error
+    # CHANGES:
+    #     * Skips logging MapNodes (since the sub-Nodes are logged)
+    #     * Adds hash and input_data_shape to status dict
+    #     * Drops duration from status dict
+    #     * Sets number of threads used to math.ceil(cpu_percent/100)
+    #     * Skips logging not-found Nodes
+    #     * Sets `None` default for start and finish
+    #     * Uses a MockLogger for the callback logger
+    #     * Modified docstring to reflect local changes
+    #     * Updated style to match C-PAC codebase
 
-    Returns
-    -------
-    None
-        this function does not return any values, it logs the node
-        status info to the callback logger
-    """
+    # ORIGINAL WORK'S ATTRIBUTION NOTICE:
+    #    Copyright (c) 2016, the CRN developers team.
+    #    All rights reserved.
+
+    #    Redistribution and use in source and binary forms, with or without
+    #    modification, are permitted provided that the following conditions are met:
+
+    #    * Redistributions of source code must retain the above copyright notice, this
+    #      list of conditions and the following disclaimer.
+
+    #    * Redistributions in binary form must reproduce the above copyright notice,
+    #      this list of conditions and the following disclaimer in the documentation
+    #      and/or other materials provided with the distribution.
+
+    #   * Neither the name of niworkflows nor the names of its
+    #      contributors may be used to endorse or promote products derived from
+    #      this software without specific prior written permission.
+
+    #    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    #    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    #    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    #    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    #    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    #    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    #    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    #    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    #    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+    #    Licensed under the Apache License, Version 2.0 (the "License");
+    #    you may not use this file except in compliance with the License.
+    #    You may obtain a copy of the License at
+
+    #        http://www.apache.org/licenses/LICENSE-2.0
+
+    #    Unless required by applicable law or agreed to in writing, software
+    #    distributed under the License is distributed on an "AS IS" BASIS,
+    #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    #    See the License for the specific language governing permissions and
+    #    limitations under the License.
+
+    # Modifications copyright (C) 2019 - 2024  C-PAC Developers
     if status != "end":
         return
 
@@ -87,6 +128,12 @@ def log_nodes_cb(node, status):
     logger.debug(json.dumps(status_dict))
 
 
+log_nodes_cb.__doc__ = f"""{_nipype_log_nodes_cb.__doc__}
+
+Modified from https://github.com/nipy/nipype/blob/5ab2fa0/nipype/utils/profiler.py#L112-L156
+"""
+
+
 class LoggingRequestHandler(socketserver.BaseRequestHandler):
     def handle(self):
         tree = {}
@@ -117,18 +164,17 @@ class LoggingRequestHandler(socketserver.BaseRequestHandler):
                                 tree[subject][node["id"]]["start"] = node["start"]
                                 tree[subject][node["id"]]["finish"] = node["finish"]
 
-                        else:
-                            if "start" in node and "finish" in node:
-                                if tree[subject][node["id"]]["hash"] == node["hash"]:
-                                    tree[subject][node["id"]]["cached"] = {
-                                        "start": node["start"],
-                                        "finish": node["finish"],
-                                    }
+                        elif "start" in node and "finish" in node:
+                            if tree[subject][node["id"]]["hash"] == node["hash"]:
+                                tree[subject][node["id"]]["cached"] = {
+                                    "start": node["start"],
+                                    "finish": node["finish"],
+                                }
 
-                                # pipeline was changed, and we have a new hash
-                                else:
-                                    tree[subject][node["id"]]["start"] = node["start"]
-                                    tree[subject][node["id"]]["finish"] = node["finish"]
+                            # pipeline was changed, and we have a new hash
+                            else:
+                                tree[subject][node["id"]]["start"] = node["start"]
+                                tree[subject][node["id"]]["finish"] = node["finish"]
 
                     except:
                         break
