2733c2441f8b0174eda1f4a3d89d9308860e9c5f
Jon Clucas <jon.clucas@childmind.org>
2024-07-08 17:15:27 -0400

    :truck: Move engine resources to own submodule

---
+++ CPAC/pipeline/engine/__init__.py
@@ -0,0 +1,50 @@
+# Copyright (C) 2021-2024  C-PAC Developers
+
+# This file is part of C-PAC.
+
+# C-PAC is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+
+# C-PAC is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+# License for more details.
+
+# You should have received a copy of the GNU Lesser General Public
+# License along with C-PAC. If not, see <https://www.gnu.org/licenses/>.
+"""C-PAC engine."""
+
+from .engine import (
+    func_outdir_ingress,
+    ingress_freesurfer,
+    ingress_output_dir,
+    ingress_pipeconfig_paths,
+    ingress_raw_anat_data,
+    json_outdir_ingress,
+    NodeBlock,
+    run_node_blocks,
+    set_iterables,
+    strip_template,
+    wrap_block,
+)
+from .resource import initiate_rpool, NodeData, ResourcePool
+
+__all__ = [
+    "func_outdir_ingress",
+    "ingress_freesurfer",
+    "ingress_raw_anat_data",
+    "ingress_output_dir",
+    "ingress_pipeconfig_paths",
+    "ingress_raw_func_data",
+    "initiate_rpool",
+    "json_outdir_ingress",
+    "NodeBlock",
+    "NodeData",
+    "ResourcePool",
+    "run_node_blocks",
+    "set_iterables",
+    "strip_template",
+    "wrap_block",
+]
