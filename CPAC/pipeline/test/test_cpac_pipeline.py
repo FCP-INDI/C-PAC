5bbc1e6af697afc45561dfd156e6a21bf66d641c
Jon Clucas <jon.clucas@childmind.org>
2024-08-23 13:47:06 -0400

    :white_check_mark: Use tmp_path for pipeline tests

--- CPAC/pipeline/test/test_cpac_pipeline.py
+++ CPAC/pipeline/test/test_cpac_pipeline.py
@@ -1,9 +1,11 @@
 """Tests for cpac_pipeline.py."""
 
-from typing import cast
+from pathlib import Path
+from typing import cast, Optional
 
 import pandas as pd
 import pytest
+from nipype.pipeline.plugins.base import PluginBase as Plugin
 
 from CPAC.pipeline.cpac_pipeline import run_workflow
 from CPAC.pipeline.nipype_pipeline_engine.plugins import MultiProcPlugin
@@ -12,12 +14,20 @@ from CPAC.utils.typing import SUB_GROUP
 
 
 @pytest.mark.parametrize("plugin", [MultiProcPlugin(), False, "MultiProc", None])
-def test_plugin_param(plugin):
+def test_plugin_param(plugin: Optional[Plugin | bool | str], tmp_path: Path):
     """We should get an KeyError from our empty `sub_dict`.
 
     If we pass a non-string to run_workflow, a TypeError should be raised.
     """
-    cfg = Configuration()
+    cfg = Configuration(
+        {
+            "pipeline_setup": {
+                "output_directory": {"path": str(tmp_path / "out")},
+                "working_directory": {"path": str(tmp_path / "work")},
+                "log_directory": {"path": str(tmp_path / "log")},
+            }
+        }
+    )
 
     with pytest.raises((TypeError, KeyError)) as e:
         sub_group = cast(SUB_GROUP, ((("", ""), pd.DataFrame([]))))
