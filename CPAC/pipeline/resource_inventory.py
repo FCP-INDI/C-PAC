38cdabe5844d4d32ec825e82a828f3db70b733f3
Jon Cluce <jon.clucas@childmind.org>
2025-01-15 15:24:07 -0500

    :sparkles: Pick up more hard-coded resources

--- CPAC/pipeline/resource_inventory.py
+++ CPAC/pipeline/resource_inventory.py
@@ -23,7 +23,7 @@ from dataclasses import dataclass, field
 import importlib
 from importlib.resources import files
 import inspect
-from itertools import chain
+from itertools import chain, product
 import os
 from pathlib import Path
 from typing import Any, cast, Iterable, Optional
@@ -91,9 +91,11 @@ class ResourceSourceList:
 
     sources: list[str] = field(default_factory=list)
 
-    def __add__(self, other: str | list[str]) -> list[str]:
+    def __add__(self, other: "str | list[str] | ResourceSourceList") -> list[str]:
         """Add a list of sources to the list."""
         if isinstance(other, str):
+            if not other:
+                other = "(dummy node)"
             other = [other]
         new_set = {*self.sources, *other}
         return sorted(new_set, key=str.casefold)
@@ -118,7 +120,9 @@ class ResourceSourceList:
         """Get the hash of the list of sources."""
         return hash(self.sources)
 
-    def __iadd__(self, other: str | list[str]) -> "ResourceSourceList":
+    def __iadd__(
+        self, other: "str | list[str] | ResourceSourceList"
+    ) -> "ResourceSourceList":
         """Add a list of sources to the list."""
         self.sources = self + other
         return self
@@ -230,7 +234,168 @@ def _flatten_io(io: list[Iterable]) -> list[str]:
     return cast(list[str], io)
 
 
-def find_directly_set_resources(package_name: str) -> dict[str, list[str]]:
+class MultipleContext(list):
+    """Subclass of list to store multilpe contexts."""
+
+
+class DirectlySetResources(ast.NodeVisitor):
+    """Class to track resources set directly, rather than through NodeBlocks."""
+
+    def __init__(self) -> None:
+        """Initialize the visitor."""
+        super().__init__()
+        self._context: dict[str, Any] = {}
+        self.dynamic_resources: dict[str, ResourceSourceList] = {}
+        self._history: dict[str, list[Any]] = {}
+        self.resources: dict[str, ResourceSourceList] = {}
+
+    def assign_resource(self, resource: str, value: str) -> None:
+        """Assign a value to a resource."""
+        target = self.dynamic_resources if r".*" in value else self.resources
+        resource = str(resource)
+        if resource not in target:
+            target[resource] = ResourceSourceList()
+        target[resource] += value
+
+    @property
+    def context(self) -> dict[str, Any]:
+        """Return the context."""
+        return self._context
+
+    @context.setter
+    def context(self, value: tuple[Iterable, Any]) -> None:
+        """Set the context."""
+        key, _value = value
+        if not isinstance(key, str):
+            for subkey in key:
+                self.context = subkey, _value
+        else:
+            self._context[key] = _value
+            if key not in self._history:
+                self._history[key] = []
+            self._history[key].append(_value)
+
+    def lookup_context(self, variable: str) -> str | MultipleContext:
+        """Plug in variable."""
+
+        def lookup() -> str | list[str]:
+            """Look up context."""
+            if variable in self.context:
+                if self.context[variable] == variable:
+                    history = list(self._history[variable])
+                    while history and history[-1] == variable:
+                        history.pop()
+                    if history:
+                        return history[-1]
+                return self.context[variable]
+            return ".*"
+
+        context = lookup()
+        if isinstance(context, list):
+            context = MultipleContext(context)
+        return context
+
+    @staticmethod
+    def handle_multiple_contexts(contexts: list[str | list[str]]) -> list[str]:
+        """Parse multiple contexts."""
+        if isinstance(contexts, list):
+            return MultipleContext(
+                [
+                    "".join(list(ctx))
+                    for ctx in product(
+                        *[
+                            context if isinstance(context, list) else [context]
+                            for context in contexts
+                        ]
+                    )
+                ]
+            )
+        return contexts
+
+    def parse_ast(self, node: Any) -> Any:
+        """Parse AST."""
+        if not isinstance(node, ast.AST):
+            if isinstance(node, str):
+                return node
+            if not isinstance(node, Iterable):
+                return str(node)
+            if isinstance(node, ast.Dict):
+                return {
+                    self.parse_ast(key): self.parse_ast(value)
+                    for key, value in dict(zip(node.keys, node.values)).items()
+                }
+            if isinstance(node, (MultipleContext, list, set, tuple)):
+                return type(node)(self.parse_ast(subnode) for subnode in node)
+        if isinstance(node, ast.FormattedValue):
+            if hasattr(node, "value") and hasattr(node.value, "id"):
+                return self.lookup_context(getattr(node.value, "id"))
+        if isinstance(node, ast.JoinedStr):
+            node_values = [self.parse_ast(value) for value in node.values]
+            if any(isinstance(value, MultipleContext) for value in node_values):
+                return self.handle_multiple_contexts(node_values)
+            return "".join(str(item) for item in node_values)
+        if isinstance(node, ast.Dict):
+            return {
+                self.parse_ast(key): self.parse_ast(value)
+                for key, value in dict(zip(node.keys, node.values)).items()
+            }
+        for attr in ["values", "elts"]:
+            if hasattr(node, attr):
+                return [self.parse_ast(subnode) for subnode in getattr(node, attr)]
+        for attr in ["value", "id"]:
+            if hasattr(node, attr):
+                return self.parse_ast(getattr(node, attr))
+        return r".*"  # wildcard for regex matching
+
+    def visit_Assign(self, node: ast.Assign) -> None:
+        """Visit an assignment."""
+        value = self.parse_ast(node.value)
+        for target in node.targets:
+            resource = self.parse_ast(target)
+            self.context = resource, value
+            # self.assign_resource(str(self.parse_ast(target)), value)
+        self.generic_visit(node)
+
+    def visit_Call(self, node: ast.Call) -> None:
+        """Visit a function call."""
+        if isinstance(node.func, ast.Attribute) and node.func.attr == "set_data":
+            value = self.parse_ast(node.args[5])
+            if hasattr(node.args[0], "value"):
+                resource: str = getattr(node.args[0], "value")
+            elif hasattr(node.args[0], "id"):
+                resource = self.lookup_context(getattr(node.args[0], "id"))
+                if isinstance(resource, MultipleContext):
+                    for resource_context in resource:
+                        self.assign_resource(resource_context, value)
+                    self.generic_visit(node)
+                    return
+            elif isinstance(node.args[0], ast.JoinedStr):
+                resource = self.parse_ast(node.args[0])
+            else:
+                self.generic_visit(node)
+                return
+            self.assign_resource(resource, value)
+        self.generic_visit(node)
+
+    def visit_For(self, node: ast.For) -> None:
+        """Vist for loop."""
+        # This is probably too specific,
+        # will need to be updated if we add more out-of-nodeblock settings.
+        target = self.parse_ast(node.target)
+        if (
+            hasattr(node.iter, "func")
+            and hasattr(node.iter.func, "value")
+            and hasattr(node.iter.func.value, "id")
+        ):
+            context = self.context.get(self.parse_ast(node.iter.func.value.id), ".*")
+            if isinstance(target, list) and isinstance(context, dict):
+                self.context = target[0], list(context.keys())
+        else:
+            self.context = target, self.parse_ast(node.iter)
+        self.generic_visit(node)
+
+
+def find_directly_set_resources(package_name: str) -> dict[str, ResourceSourceList]:
     """Find all resources set explicitly via :pyy:method:`~CPAC.pipeline.engine.ResourcePool.set_data`.
 
     Parameters
@@ -243,28 +408,31 @@ def find_directly_set_resources(package_name: str) -> dict[str, list[str]]:
     dict
         A dictionary containing the name of the resource and the name of the functions that set it.
     """
-    resources: dict[str[list[str]]] = {}
+    resources: dict[str, ResourceSourceList] = {}
+    dynamic_resources: dict[str, ResourceSourceList] = {}
     for dirpath, _, filenames in os.walk(str(files(package_name))):
         for filename in filenames:
             if filename.endswith(".py"):
                 filepath = os.path.join(dirpath, filename)
                 with open(filepath, "r", encoding="utf-8") as file:
                     tree = ast.parse(file.read(), filename=filepath)
-                    for node in ast.walk(tree):
-                        if isinstance(node, ast.Call) and isinstance(
-                            node.func, ast.Attribute
-                        ):
-                            if node.func.attr == "set_data":
-                                try:
-                                    resource: str = ast.literal_eval(node.args[0])
-                                    if resource not in resources:
-                                        resources[resource] = []
-                                    resources[resource].append(
-                                        ast.literal_eval(node.args[-1])
-                                    )
-                                except ValueError:
-                                    # The resource name or function name is not a literal, so this `set_data` is a dynamic call
-                                    pass
+                    directly_set = DirectlySetResources()
+                    directly_set.visit(tree)
+                    for resource in directly_set.resources:
+                        if resource not in resources:
+                            resources[resource] = ResourceSourceList()
+                        resources[resource] += directly_set.resources[resource]
+                    for resource in directly_set.dynamic_resources:
+                        if resource not in dynamic_resources:
+                            dynamic_resources[resource] = ResourceSourceList()
+                        dynamic_resources[resource] += directly_set.dynamic_resources[
+                            resource
+                        ]
+    # for dynamic_key, dynamic_value in dynamic_resources.items():
+    #     dynamic_resource = re.compile(dynamic_key)
+    #     for resource in resources.keys():
+    #         if dynamic_resource.search(resource):
+    #             resources[resource] += dynamic_value
     return resources
 
 
