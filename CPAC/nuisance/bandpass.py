04f0d5257e107bf0db838b1c08173fad098c0af5
birajstha:construction_worker::penguin <birajstha@gmail.com>
2025-05-27 12:55:13 -0400

    adding to changelog

--- CPAC/nuisance/bandpass.py
+++ CPAC/nuisance/bandpass.py
@@ -8,41 +8,55 @@ from scipy.fftpack import fft, ifft
 
 
 def ideal_bandpass(data, sample_period, bandpass_freqs):
-    # Derived from YAN Chao-Gan 120504 based on REST.
+    """
+    Apply ideal bandpass filtering to a 1D time series data using FFT. Derived from YAN Chao-Gan 120504 based on REST.
+
+    Parameters
+    ----------
+    data : NDArray
+        1D time series data to be filtered.
+    sample_period : float
+        Length of sampling period in seconds.
+    bandpass_freqs : tuple
+        Tuple containing the bandpass frequencies (LowCutoff, HighCutoff).
+
+    Returns
+    -------
+    NDArray
+        Filtered time series data.
+
+    """
     sample_freq = 1.0 / sample_period
     sample_length = data.shape[0]
+    nyquist_freq = sample_freq / 2.0
 
-    data_p = np.zeros(int(2 ** np.ceil(np.log2(sample_length))))
+    # Length of zero-padded data for efficient FFT
+    N = int(2 ** np.ceil(np.log2(len(data))))
+    data_p = np.zeros(N)
     data_p[:sample_length] = data
 
     LowCutoff, HighCutoff = bandpass_freqs
 
     if LowCutoff is None:  # No lower cutoff (low-pass filter)
         low_cutoff_i = 0
-    elif LowCutoff > sample_freq / 2.0:
+    elif LowCutoff > nyquist_freq:
         # Cutoff beyond fs/2 (all-stop filter)
-        low_cutoff_i = int(data_p.shape[0] / 2)
+        low_cutoff_i = int(N / 2)
     else:
-        low_cutoff_i = np.ceil(LowCutoff * data_p.shape[0] * sample_period).astype(
-            "int"
-        )
+        low_cutoff_i = np.ceil(LowCutoff * N * sample_period).astype("int")
 
-    if HighCutoff > sample_freq / 2.0 or HighCutoff is None:
+    if HighCutoff > nyquist_freq or HighCutoff is None:
         # Cutoff beyond fs/2 or unspecified (become a highpass filter)
-        high_cutoff_i = int(data_p.shape[0] / 2)
+        high_cutoff_i = int(N / 2)
     else:
-        high_cutoff_i = np.fix(HighCutoff * data_p.shape[0] * sample_period).astype(
-            "int"
-        )
+        high_cutoff_i = np.fix(HighCutoff * N * sample_period).astype("int")
 
     freq_mask = np.zeros_like(data_p, dtype="bool")
     freq_mask[low_cutoff_i : high_cutoff_i + 1] = True
-    freq_mask[data_p.shape[0] - high_cutoff_i : data_p.shape[0] + 1 - low_cutoff_i] = (
-        True
-    )
+    freq_mask[N - high_cutoff_i : N + 1 - low_cutoff_i] = True
 
     f_data = fft(data_p)
-    f_data[freq_mask is not True] = 0.0
+    f_data[~freq_mask] = 0.0
     return np.real_if_close(ifft(f_data)[:sample_length])
 
 
