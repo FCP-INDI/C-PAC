35547c468c78e1ff5e78a97637b774cfa63a6f54
birajstha:construction_worker::penguin <birajstha@gmail.com>
2025-07-07 14:06:44 -0400

    fixing ants.py for missing pkgr_fn

--- CPAC/anat_preproc/ants.py
+++ CPAC/anat_preproc/ants.py
@@ -295,23 +295,12 @@ def init_brain_extraction_wf(  # noqa: PLR0913
         init_aff.inputs.search_grid = (40, (0, 40, 40))
 
     # Set up spatial normalization
-    settings_file = (
-        "antsBrainExtraction_%s.json"
-        if use_laplacian
-        else "antsBrainExtractionNoLaplacian_%s.json"
-    )
-    with as_file(
-        files("CPAC").joinpath(
-            f"anat_preproc/data/{settings_file}{normalization_quality}"
-        )
-    ) as _f:
-        norm = pe.Node(
-            Registration(from_file=str(_f)),
-            name="norm",
-            n_procs=omp_nthreads,
-            mem_gb=1.7,
-            mem_x=(1233286593342025 / 151115727451828646838272, "moving_image"),
-        )
+    settings_file = (f'antsBrainExtraction_{normalization_quality}.json' if use_laplacian else f'antsBrainExtractionNoLaplacian_{normalization_quality}.json')
+    norm = pe.Node(Registration(from_file=as_file(files('CPAC.anat_preproc').joinpath('data').joinpath(settings_file))),
+        name='norm',
+        n_procs=omp_nthreads,
+        mem_gb=1.7,
+        mem_x=(1233286593342025 / 151115727451828646838272, 'moving_image'))
     norm.inputs.float = use_float
     fixed_mask_trait = "fixed_image_mask"
     if _ants_version and parseversion(_ants_version) >= Version("2.2.0"):
